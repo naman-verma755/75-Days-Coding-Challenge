cac425103970080d5ddaba05d2cc705d703b1175
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-03-31 14:03:41 +0530

    Time: 32 ms (99.77%), Space: 10.7 MB (90.57%) - LeetHub

---
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+        
+        int n = s.length();
+        int dp[n][n];
+//          for(int i =0; i<n; i++) {
+//             for(int j = 0; j<n;j++)
+//                 dp[i][j] = 0;
+           
+//         }
+        for(int i = 0; i < n; i++)
+            dp[i][i] = 1;
+        for(int l = 1; l < n; l++) {
+            for(int j = 0, k = j+l; k < n; k++, j++) {
+                // cout<<j<<k<<" ";
+                if(s[j] == s[k])
+                {
+                    if(l == 1)
+                        dp[j][k] = 2;
+                    else
+                        dp[j][k] = 2+dp[j+1][k-1];
+                }
+                else
+                    dp[j][k] = max(dp[j][k-1], dp[j+1][k]);
+            }
+        }
+        // for(int i =0; i<n; i++) {
+        //     for(int j = 0; j<n;j++)
+        //         cout<<dp[i][j]<<" ";
+        //     cout<<endl;
+        // }
+        return dp[0][n-1];
+        
+    }
+};
