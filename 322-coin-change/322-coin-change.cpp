b2e048fc72d2837b06708b7ce02fe8875f3f8e14
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-04-07 21:06:11 +0530

    Time: 420 ms (7.01%), Space: 44.9 MB (21.41%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
-    int findMinCoinChange(int i, vector<int>& coins, int amount, vector<vector<int>>&v) {
-        
-        // cout<<amount<<" ";
-        
+
+    int coinChange(vector<int>& coins, int amount) {
         if(amount == 0)
             return 0;
-        if(i >= coins.size() || amount < 0)
-            return INT_MAX;
-        if(v[i][amount] != -1)
-            return v[i][amount];
+        vector<vector<int>>dp(coins.size(), vector<int>(amount+1,-1));
+        int size = coins.size();
+        // dp[coins.size()][amount+1];
+        for(int i = 0; i < size; i++) {
+            for(int j = 1; j <= amount; j++) {
+                int count1 = INT_MAX, count2 = INT_MAX;
+                if(i > 0)
+                    count1 = dp[i-1][j];
+                if(j-coins[i] == 0)
+                    count2 = 1;
+                if(j-coins[i] > 0 && dp[i][j-coins[i]] != INT_MAX) {
+                  
+                        count2 = 1+dp[i][j-coins[i]];
+                }
+                    
+                dp[i][j] = min(count1, count2);
+            }
+        }
         
-        int count1 = findMinCoinChange(i+1, coins, amount, v);
-        int count2 = findMinCoinChange(i, coins, amount-coins[i], v);
-        if(count2 != INT_MAX)
-            count2+=1;
-        return v[i][amount] = min(count1, count2);
         
-    }
-    int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>>v(coins.size()+1, vector<int>(amount+1,-1));
-        
-        int k = findMinCoinChange(0, coins, amount, v);
-        if(k == INT_MAX)
+      
+        if(dp[size-1][amount] == INT_MAX)
             return -1;
-        return k;
+        return dp[size-1][amount] ;
         
     }
 };
