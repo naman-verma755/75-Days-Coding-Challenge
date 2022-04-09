accf80d8567dc93b5642ba6d2afb012cb3298c58
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-04-09 14:58:03 +0530

    Time: 0 ms (100.00%), Space: 6 MB (55.61%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -3,15 +3,19 @@ public:
     int climbStairs(int n) {
                 if(n <= 2)
             return n;
-        int dp[n+1];
-        dp[1] = 1;
-        dp[2] = 2;
-
+       
+        int a = 1;
+        int b = 2;
+         
         
         for(int i = 3; i <= n; i++)
-            dp[i] = dp[i-1]+dp[i-2];
+            {
+            int c = a + b;
+            swap(a,b);
+            b = c;
+        }
         
-        return dp[n];
+        return b;
     }
     
 };
