dda472543255ec6583579280b332c38ab14340b9
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-04-11 19:46:33 +0530

    Time: 12 ms (88.00%), Space: 6.4 MB (96.50%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -8,14 +8,9 @@ public:
         // int dp[size1+1][size2+1];
         
         vector<int>prev(size2+1,0);
-        // for(int i = 0; i <= size2; i++) {
-        //     dp[0][i] = 0;
-        // }
-        // for(int i = 0; i <= size1; i++) {
-        //     dp[i][0] = 0;
-        // }
+       vector<int>curr(size2+1,0);
         for(int i = 1; i <= size1; i++) {
-            vector<int>curr(size2+1,0);
+            
             for(int j = 1; j <= size2; j++) {
                 if(text1[i-1] == text2[j-1]){
                     curr[j] = 1 + prev[j-1];
