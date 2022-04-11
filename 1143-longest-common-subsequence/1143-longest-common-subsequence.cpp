4736a33b7ec0776da27c2d657939f6569410d0dd
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-04-11 19:45:47 +0530

    Time: 16 ms (78.73%), Space: 12.7 MB (53.92%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -5,26 +5,30 @@ public:
         int size1 = text1.length();
         int size2 = text2.length();
        
-        int dp[size1+1][size2+1];
-        for(int i = 0; i <= size2; i++) {
-            dp[0][i] = 0;
-        }
-        for(int i = 0; i <= size1; i++) {
-            dp[i][0] = 0;
-        }
+        // int dp[size1+1][size2+1];
+        
+        vector<int>prev(size2+1,0);
+        // for(int i = 0; i <= size2; i++) {
+        //     dp[0][i] = 0;
+        // }
+        // for(int i = 0; i <= size1; i++) {
+        //     dp[i][0] = 0;
+        // }
         for(int i = 1; i <= size1; i++) {
+            vector<int>curr(size2+1,0);
             for(int j = 1; j <= size2; j++) {
                 if(text1[i-1] == text2[j-1]){
-                    dp[i][j] = 1 + dp[i-1][j-1];
+                    curr[j] = 1 + prev[j-1];
                 }
                 else {
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                    curr[j] = max(prev[j], curr[j-1]);
                 }
             }
+            prev = curr;
         }
         
         
-       return dp[size1][size2];
+       return prev[size2];
         
     }
 };
