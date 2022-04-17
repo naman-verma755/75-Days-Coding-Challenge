b2da7d1677c0b6605c7af6ee241313a877671cb1
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-04-17 19:10:16 +0530

    Time: 452 ms (27.61%), Space: 43.3 MB (80.62%) - LeetHub

--- 472-concatenated-words/472-concatenated-words.cpp
+++ 472-concatenated-words/472-concatenated-words.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    bool func( string word, map<string, int>&mp) {
+    bool func( string word, map<string, int>&mp, map<string, int>&dp) {
         // cout<<word<<" ";
         int n = word.length();
         if( n == 0)
             return true;
-        
+        if(dp.count(word))
+            return dp[word];
         string s = "";
         for(int j = 0; j < n; j++) {
             s+=word[j];
-            if(mp.count(s) && func( word.substr(j+1, n), mp )) {
-                return true;
+            if(mp.count(s) && func( word.substr(j+1, n), mp, dp )) {
+                return dp[word] = true;
             }
         }
         return false;
     }
     vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
-        map<string, int>mp, visited;
+        map<string, int>mp, visited, dp;
         
         for(int i = 0; i < words.size(); i++) {
             mp[words[i]] = 1;
@@ -32,7 +33,7 @@ public:
                 s += words[i][j];
                 
                 if(mp.count(s) && j < words[i].length()-1) {
-                    if(func( words[i].substr(j+1, words[i].length()), mp))
+                    if(func( words[i].substr(j+1, words[i].length()), mp, dp))
                         if(!visited[words[i]]) {
                             ans.push_back(words[i]);
                             visited[words[i]]++;
