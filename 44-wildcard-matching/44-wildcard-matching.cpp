69a370291a32e7580a68b5268d368bab47734e9c
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-04-18 20:19:30 +0530

    Time: 88 ms (43.98%), Space: 9.6 MB (66.22%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -1,75 +1,60 @@
 class Solution {
 public:
-    bool recur(int i, int j, string s, string p, int n, int m, vector<vector<int>>&dp) {
-        // cout<<i<<" "<<j<<endl;
+    bool func(int i, int j, string s, string p, vector<vector<int>>&dp) {
+        int n = s.length();
+        int m = p.length();
         
         if(i >= n && j >= m)
             return true;
-        if(j >= m && i < n )
+        if(i < n && j >= m)
             return false;
         if(i >= n && j < m) {
-            for(int ii = j; ii < m; ii++) {
-                if(p[ii] != '*')
+            for(int k = j; k < m; k++) {
+                if(p[k] != '*')
                     return false;
-                
             }
             return true;
         }
-            // return true;
+        
         
         if(dp[i][j] != -1)
             return dp[i][j];
-        if(s[i] == p[j] || p[j] == '?') {
-            return dp[i][j] = recur(i+1, j+1, s, p, n, m, dp);
+        if(s[i] == p[j] || p[j] == '?')  {
+            return dp[i][j] = func(i+1, j+1, s, p, dp);
         }
-         if(p[j] == '*') {
-            return dp[i][j] =(recur(i+1, j, s, p, n, m, dp) || recur(i, j+1, s, p, n, m, dp));
+        if(p[j] == '*') {
+            return dp[i][j] = func(i+1, j, s, p, dp) || func(i, j+1, s, p, dp);
         }
-      
-        return dp[i][j] = false;
+    
+            return dp[i][j] = false;
     }
     bool isMatch(string s, string p) {
         int n = s.length();
         int m = p.length();
-         vector<vector<bool>>dp(m+1, vector<bool>(n+1, false));
-        
-        dp[0][0] = true;
-        bool fi = true;
+        vector<vector<bool>>dp(m+1, vector<bool>(n+1,false));
         for(int i = 1; i <= m; i++) {
             
-            // for(int j = 1; j <= i; j++)
-            // {
-                if(p[i-1] != '*') {
-                    
-                    break;
-                }
-          
-            dp[i][0] = fi;
+            if(p[i-1] != '*')
+                break;
+            dp[i][0] = true;
+            
         }
-        
+        dp[0][0] = true;
         for(int i = 1; i <= m; i++) {
-            for(int j = 1; j <= n; j++) {
-                
-        if(s[j-1] == p[i-1] || p[i-1] == '?') {
-             dp[i][j] = dp[i-1][j-1];
-        }
-        else if(p[i-1] == '*') {
-             dp[i][j] =dp[i-1][j] || dp[i][j-1];
-        }
-        else
-        dp[i][j] = false;
-                
+            for(int j = 1; j <=n; j++) {
+                if(s[j-1] == p[i-1] || p[i-1] == '?'){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else if(p[i-1] == '*') {
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                }
+                else
+                    dp[i][j] = false;
             }
         }
         
-        // for(int i = 0; i <= m; i++) {
-        //     for(int j = 0; j <= n; j++) {
-        //         cout<<dp[i][j]<<" ";
-        //     }cout<<endl;
-        // }
         return dp[m][n];
         
+        
     }
-// "a*ab"
-// "baaabab"
 };
