4869aa2106edd5893230e968ca90219295f8ac65
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-04-19 20:37:07 +0530

    Time: 7 ms (86.40%), Space: 7.4 MB (46.58%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -3,14 +3,14 @@ public:
     bool isAnagram(string s, string t) {
         
         int dp1[27] = {};
-        int dp2[27] = {};
+        // int dp2[27] = {};
         for(auto it : s)
             dp1[it-'a'] ++;
         for(auto it : t) 
-            dp2[it-'a'] ++;
+            dp1[it-'a'] --;
         for(int i = 0; i < 27; i++)
         {
-            if(dp1[i] != dp2[i])
+            if(dp1[i] != 0)
                 return false;
         }
         return true;
