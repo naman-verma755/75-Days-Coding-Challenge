ddace10ed5bd537bf99ae9417a21ef28e5d41626
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-04-26 22:15:12 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -9,66 +9,66 @@ class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
-	int find(int node, int parent[]) {
-	    if(node == parent[node])
-	    return node;
+	
+	int find(int u, int parent[]) {
+	    if(u == parent[u])
+	    return u;
 	    
-	    return parent[node] = find(parent[node], parent);
+	    return parent[u] = find(parent[u], parent);
 	}
-	void unionOf(int u, int v, int rank[], int parent[]) {
-	    if(rank[u] > rank[v]) {
-	        parent[v] = u;
-	    }
-	    else if(rank[u] < rank[v])
+	void unionOf(int u, int v, int parent[], int rank[]) {
+	    
+	    if(rank[u] < rank[v]) 
+	    parent[u] = parent[v];
+	    else if(rank[u] > rank[v])
+	    parent[v] = u;
+	    else
 	    {
-	        parent[u] = v;
-	    }
-	    else{
-	        rank[u]++;
+	        rank[u] ++;
 	        parent[v] = u;
 	    }
 	}
     int spanningTree(int V, vector<vector<int>> adj[])
     {
         // code here
-        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>>pq;
-        for(int i = 0; i < V; i++) {
-            // cout<<i<<"="<<endl;
-            for(auto it : adj[i])
-            {
-                if(i < it[0]) {
-                    pq.push({it[1],{i,it[0]}});
-                }
-            }
+       priority_queue<pair<int, pair< int, int>>, vector<pair<int, pair< int, int>>>, greater<pair<int, pair< int, int>>>>pq;
+       
+       for(int i = 0; i < V; i++) {
+           for(auto it : adj[i]) {
+               int wt = it[1];
+               int v = it[0];
+               pq.push({wt, {i, v}});
+           }
+       }
+       
+       int parent[V];
+       int rank[V];
+       for(int i = 0; i < V; i++) {
+           parent[i] = i;
+           rank[i] = 0;
+       }
+       int sum = 0;
+       while(!pq.empty()) {
            
-        }
-        int weightOfTree = 0;
-        int parent[V];
-        int rank[V];
-        for(int i = 0; i < V; i++)
-        {
-            parent[i] = i;
-            rank[i] = 0;
-        }
-        for(int i =0 ; i < V-1; ) {
-            
-            auto it = pq.top();
-            pq.pop();
-            int wt = it.first;
-            int u = it.second.first;
-            int v = it.second.second;
-            
-            int pt1 = find(u, parent);
-            int pt2 = find(v, parent);
-            // cout<<u<<" "<<pt1<<v<<" "<<pt2<<endl;
-            if(pt1 == pt2)
-            continue;
-            i++;
-            weightOfTree += wt;
-            unionOf(pt1, pt2, rank, parent);
-        }
-        return weightOfTree;;
+           auto it = pq.top();
+           pq.pop();
+           int wt = it.first;
+           int u = it.second.first;
+           int v = it.second.second;
+           
+           int parentu = find(u, parent);
+           int parentv = find(v, parent);
+           
+           if(parentu == parentv)
+           continue;
+           
+           sum += wt;
+           unionOf(parentu, parentv, parent, rank);
+       }
         
+        
+        
+       return sum;
         
         
         
