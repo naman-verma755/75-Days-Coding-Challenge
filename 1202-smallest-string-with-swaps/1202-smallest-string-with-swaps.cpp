33e98ac36b5e17e2a1d5389da10b51cbefc177b3
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-04-27 12:36:49 +0530

    Time: 175 ms (85.81%), Space: 52.4 MB (61.26%) - LeetHub

--- 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.cpp
+++ 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.cpp
@@ -1,50 +1,64 @@
 class Solution {
 public:
+    int find(int u, int parent[]) {
+        if(u == parent[u])
+            return u;
+        
+        return parent[u] = find(parent[u], parent);
+    }
     
-    void dfs(int node, vector<int>adj[], vector<int>&indices, vector<char>&characters, bool visited[], string &s) {
-        
-          visited[node] = true;
-          characters.push_back(s[node]);
-          indices.push_back(node);
-        
-          for(auto it : adj[node]) {
-              if(!visited[it]) {
-                  dfs(it, adj, indices, characters, visited, s);
-              }
-          }
+    void unionOf(int u, int v, int parent[], int rank[]) {
+        if(rank[u] < rank[v]) {
+            parent[u] = v;
+        }
+        else if(rank[v] < rank[u]) {
+            parent[v] = u;
+        }
+        else {
+            rank[u] ++;
+            parent[v] = u;
+        }
     }
     string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
         
+        
         int size = s.length();
-        vector<int>adj[size];
+        int parent[size];
+        int rank[size];
+        for(int i = 0; i < size; i++) {
+            parent[i] = i;
+            rank[i] = 0;
+        }
         
         for(int i = 0; i < pairs.size(); i++) {
             
-              int u = pairs[i][0];
-              int v = pairs[i][1];
-              adj[u].push_back(v);
-              adj[v].push_back(u);
-        }
-        
-        bool visited[size];
-        memset(visited, false ,sizeof(visited));
-        for(int i = 0; i < size; i++) {
+            int parentu = find(pairs[i][0], parent);
+            int parentv = find(pairs[i][1], parent);
             
-            if(!visited[i]) {
-                
-                vector<char>characters;
-            vector<int>indices;
-                
-                dfs(i, adj, indices, characters, visited, s);
-                sort(characters.begin(), characters.end());
-                sort(indices.begin(), indices.end());
-                
-                for(int i = 0; i < indices.size(); i++) {
-                    s[indices[i]] = characters[i];
-                }
+            if(parentu == parentv)
+                continue;
+            
+            unionOf(parentu, parentv, parent,rank);
+        }
+        map<int , vector<int>>mp;
+        for(int i = 0; i < size; i++)
+        {
+            int root = find(i, parent);
+            mp[root].push_back(i);
+        }
+        string str(s.length(), ' ');
+        for(auto it : mp) {
+            vector<int>indices = it.second;
+            vector<char>characters;
+            for(int i = 0; i < indices.size(); i++)
+                characters.push_back(s[indices[i]]);
+            sort(characters.begin(), characters.end());
+            
+            for(int i = 0; i < indices.size(); i++) {
+                str[indices[i]] = characters[i];
             }
             
         }
-        return s;
+        return str;
     }
 };
