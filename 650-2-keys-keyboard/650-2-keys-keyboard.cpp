548849ab10797fac6e01b523238363a8fd855fd0
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-04-28 16:36:18 +0530

    Time: 55 ms (16.75%), Space: 159.5 MB (7.39%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/650-2-keys-keyboard.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
-    int func(int clip, int length, int n) {
+    int func(int clip, int length, int n,  vector<vector<int>>&dp) {
         
         if(length == n)
             return 0;
         
         if(length > n)
             return INT_MAX;
-        
-        int a =  func(clip, length+clip, n);
-        int b =  func(length, length+length, n);
+        if(dp[clip][length] != -1)
+            return dp[clip][length];
+        int a =  func(clip, length+clip, n, dp);
+        int b =  func(length, length+length, n, dp);
         if( a == INT_MAX && b == INT_MAX)
-            return a;
+            return dp[clip][length] = a;
         if(a == INT_MAX && b != INT_MAX)
-            return b+2;
+            return dp[clip][length] = b+2;
         if(a != INT_MAX && b == INT_MAX)
-            return a+1;
+            return dp[clip][length] = a+1;
         
          
-        return min(a+1,b+2);
+        return dp[clip][length] = min(a+1,b+2);
     }
     int minSteps(int n) {
         if(n == 1)
             return 0;
+        vector<vector<int>>dp(n+3, vector<int>(n+3, -1));
         
-        int count = func(1, 1, n)+1;
+        int count = func(1, 1, n, dp)+1;
         return count;
     }
 };
