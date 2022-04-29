170936e730648722127296e9c60b4014a6b6f35f
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-04-29 22:09:36 +0530

    Time: 17 ms (15.19%), Space: 13.6 MB (95.20%) - LeetHub

---
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        
+        
+        int maxi = nums[0];
+        int mini = nums[0];
+        int maximumProduct = nums[0];
+        
+        for(int i = 1; i < nums.size(); i++) {
+            
+            if(nums[i] < 0)
+                swap(maxi, mini);
+            
+            maxi = max(nums[i], (nums[i]*maxi));
+            mini = min(nums[i], (nums[i]*mini));
+            maximumProduct = max(maximumProduct, max(maxi, mini));
+        }
+        return maximumProduct;
+    }
+};
