9d683b0396abd98a820ec4dd13ed33bb6cdba846
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-04-29 21:12:28 +0530

    Time: 49 ms (71.67%), Space: 7.7 MB (77.45%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -1,60 +1,34 @@
 class Solution {
 public:
-    bool func(int i, int j, string s, string p, vector<vector<int>>&dp) {
-        int n = s.length();
-        int m = p.length();
-        
-        if(i >= n && j >= m)
-            return true;
-        if(i < n && j >= m)
-            return false;
-        if(i >= n && j < m) {
-            for(int k = j; k < m; k++) {
-                if(p[k] != '*')
-                    return false;
-            }
-            return true;
-        }
+    bool isMatch(string str,string pattern) {
         
         
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        if(s[i] == p[j] || p[j] == '?')  {
-            return dp[i][j] = func(i+1, j+1, s, p, dp);
-        }
-        if(p[j] == '*') {
-            return dp[i][j] = func(i+1, j, s, p, dp) || func(i, j+1, s, p, dp);
-        }
-    
-            return dp[i][j] = false;
-    }
-    bool isMatch(string s, string p) {
-        int n = s.length();
-        int m = p.length();
-        vector<vector<bool>>dp(m+1, vector<bool>(n+1,false));
-        for(int i = 1; i <= m; i++) {
-            
-            if(p[i-1] != '*')
-                break;
-            dp[i][0] = true;
-            
-        }
+       int pLength = pattern.length();
+       int strLength = str.length();
+        
+        bool dp[pLength+1][strLength+1];
+        memset(dp, false, sizeof(dp));
         dp[0][0] = true;
-        for(int i = 1; i <= m; i++) {
-            for(int j = 1; j <=n; j++) {
-                if(s[j-1] == p[i-1] || p[i-1] == '?'){
-                    dp[i][j] = dp[i-1][j-1];
-                }
-                else if(p[i-1] == '*') {
-                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
-                }
+        
+        for(int i = 1; i <= pLength; i++) {
+            if(pattern[i-1] == '*')
+            dp[i][0] = true;
+            else
+            break;
+        }
+        for(int i = 1; i <= pLength; i++) {
+            for(int j = 1; j <= strLength; j++) {
+                if(pattern[i-1] == str[j-1] || pattern[i-1] == '?')
+                dp[i][j] = dp[i-1][j-1];
+                else if(pattern[i-1] == '*') 
+                dp[i][j] = dp[i-1][j] || dp[i][j-1];
                 else
-                    dp[i][j] = false;
+                dp[i][j] = false;
             }
         }
-        
-        return dp[m][n];
-        
+        return dp[pLength][strLength];
+        // return findMatch(0, 0, pattern, str);
         
     }
+    
 };
