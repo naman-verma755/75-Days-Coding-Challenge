a3b4f59ec9c7c01c6cbe7c5783b58e1ce68172fd
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-05-03 19:42:58 +0530

    Time: 18 ms (49.53%), Space: 9.5 MB (66.83%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
-    
-    
-    bool func( string s, map<string, int>&mp, map<string, bool>&dp ) {
+    bool find(string s, map<string, int>&mp, map<string, bool>&dp) {
         
-        // cout<<index<<" ";
         if(s.length() == 0)
             return true;
-        if(dp.count(s))
-            return dp[s] ;
-        string res = "";
-        for(int i = 0; i < s.length(); i++) {
-            res += s[i];
-            // cout<<res<<" ";
-            if(mp.count(res) && func( s.substr(i+1, s.length()), mp, dp))
-            return dp[res] =  true;
-        }
-        return dp[s] = false;
-            
-            
         
+        if(dp.count(s))
+            return dp[s];
+        // cout<<s<<" ";
+        string res = "";
+        
+        for(int i = 0; i < s.length(); i++) {
+            
+            res += s[i];
+            if(mp.count(res) && find(s.substr(i+1, s.length()), mp, dp))
+               return dp[res] = true;
+               
+        }
+        return dp[res] = false;
     }
     bool wordBreak(string s, vector<string>& wordDict) {
         
         map<string, int>mp;
         map<string, bool>dp;
-        for(int i = 0; i < wordDict.size(); i++) {
-            mp[wordDict[i]]++;
-        }
-        // for(auto it : mp)
-        //     cout<<it.first<<" ";
+        for(string &word : wordDict)
+            mp[word] = 1;
         
-        return func( s, mp, dp);
-        
+        if(find(s, mp, dp))
+            return true;
+        return false;
     }
 };
