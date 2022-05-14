b3856f8816e1f42888e7949e2bfde8c7991a5ec6
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-05-14 12:32:57 +0530

    Time: 33 ms (46.91%), Space: 11.9 MB (63.30%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,23 +1,44 @@
 class Solution {
 public:
+    int func(int i, int sum, int amount, vector<int>&a) {
+        
+        if(i >= a.size() || sum > amount)
+            return 0;
+        
+        if(sum == amount)
+            return 1;
+        
+        return func(i, sum+a[i], amount, a)+func(i+1, sum, amount, a);
+            
+        
+    }
     int change(int amount, vector<int>& coins) {
         
+        // return func(0, 0, amount, coins);
         int size = coins.size();
-        int dp[size][amount+1];
-        for(int i = 0; i < size; i++)
-            dp[i][0] = 1;
+        int dp[size+1][amount+1];
         
-        for(int i = 0; i < size; i++) {
+        for(int i = 0; i <= size; i++) {
+         
+            dp[i][0] = 1;
+        }
+        for(int i = 0; i <= amount; i++) {
+            dp[0][i] = 0;
+            // dp[i][0] = 1;
+        }
+        dp[0][0] = 1;
+        
+        for(int i = 1; i <= size; i++) {
             for(int j = 1; j <= amount; j++) {
-                int ways = 0;
-                if(i > 0)
-                    ways += dp[i-1][j];
-                if(j-coins[i] >= 0)
-                    ways += dp[i][j-coins[i]];
-                dp[i][j] = ways;
+                int sum = 0;
+                if(j-coins[i-1] >= 0)
+                    sum += dp[i][j-coins[i-1]];
+                sum += dp[i-1][j];
+                dp[i][j] = sum;
             }
         }
-        return dp[size-1][amount];
+        
+        return dp[size][amount];
         
     }
 };
