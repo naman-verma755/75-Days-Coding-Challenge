99f62dc82ea97f229053cea108c7be0e2ca443c2
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-05-15 21:01:40 +0530

    Added solution - LeetHub

--- Longest Common Substring - GFG/longest-common-substring.cpp
+++ Longest Common Substring - GFG/longest-common-substring.cpp
@@ -5,34 +5,41 @@ using namespace std;
  // } Driver Code Ends
 class Solution{
     public:
-    
+    int func(int i, int j, string s1, string s2) {
+        if(i >= s1.length() || j >= s2.length())
+        return 0;
+        
+        
+        int k = 0;
+        
+        if(s1[i] == s2[j])
+        k = 1+func(i+1, j+1, s1, s2);
+        
+        k = max(k, max(func(i+1, j, s1, s2), func(i, j+1, s1, s2)));
+        
+        return k;
+    }
     int longestCommonSubstr (string S1, string S2, int n, int m)
     {
-        // your code 
-        int n1 = S1.length(), n2 = S2.length();
-        int dp[n1][n2];
-        int longest = INT_MIN;
-        for(int i=0; i<n1; i++)
-        {
-            for(int j=0; j<n2; j++)
-            {
-                if(S1[i] == S2[j] && (i == 0 || j==0))
-                {
-                    dp[i][j] = 1;
-                }
-                else if(S1[i] == S2[j] )
-                {
-                    dp[i][j] = 1+dp[i-1][j-1];
-                }
+        // your code here
+        // return func(0, 0, S1, S2);
+        int dp[S1.length()+1][S2.length()+1];
+        for(int i = 0; i < S1.length(); i++) 
+        dp[i][0] = 0;
+        for(int i = 0; i < S2.length(); i++) 
+        dp[0][i] = 0;
+        int maxi = 0;
+        for(int i = 1; i <= S1.length(); i++) {
+            for(int j = 1; j <= S2.length(); j++) {
+                if(S1[i-1] == S2[j-1])
+                dp[i][j] = 1+dp[i-1][j-1];
                 else
-                {
-                    dp[i][j] = 0;
-                }
-                longest = max(longest,dp[i][j]);
+                dp[i][j] = 0;
                 
+                maxi = max(maxi, dp[i][j]);
             }
         }
-        return longest;
+        return maxi;
         
     }
 };
