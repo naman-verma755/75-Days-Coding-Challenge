eb5eb01f9758c3bfad4cd2a44eb83d424951d3a2
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-05-25 12:33:09 +0530

    Time: 79 ms (79.18%), Space: 34.5 MB (47.15%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.cpp
+++ 621-task-scheduler/621-task-scheduler.cpp
@@ -2,44 +2,20 @@ class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
         
-        priority_queue<pair<int, char>>pq;
-    
-        
-        int count = 0;
-        
-        int freq[27] = {};
-        for(char &ch : tasks)
-            freq[ch-'A']++;
-        
-        for(int i = 0; i < 27; i++) {
-            if(freq[i] > 0)
-                pq.push({freq[i],('A'+i)});
+        int maxFreq = 0;
+        map<char, int>mp;
+        for(char &ch : tasks) {
+            mp[ch] ++;
+            maxFreq = max(maxFreq, mp[ch]);
         }
         
-        while(!pq.empty()) {
-                vector<pair<int, char>>v;
-           int r = 0;
-            while(!pq.empty() &&  r <= n) {
-                if(pq.size() == 1) {
-                    
-                }
-                pair<int, char>pr = pq.top();
-                pq.pop();
-                if(pr.first-1 > 0)
-                v.push_back({pr.first-1, pr.second});
-                count++;
-                
-                r++;
-            }
-            if(v.size() > 0)
-            count += (n-r+1);
-            
-            for(auto it : v) {
-                pq.push({it.first, it.second});
-            }
-         // cout<<count<<" ";            
-            
+        int ans = (maxFreq-1)*(n+1);
+        for(auto it : mp) {
+            if(it.second == maxFreq)
+                ans++;
         }
-        return count;
+        
+       
+        return max((int)tasks.size(), ans);
     }
 };
