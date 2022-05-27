c7ba7950342c0e976359a99baf5f44e93785aecb
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-05-27 13:33:22 +0530

    Time: 29 ms (53.07%), Space: 11.9 MB (56.89%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,44 +1,45 @@
 class Solution {
 public:
-    int func(int i, int sum, int amount, vector<int>&a) {
+//     int findCombinations(int index, int amount, vector<int>&coins, vector<vector<int>>dp) {
         
-        if(i >= a.size() || sum > amount)
-            return 0;
+//         if(amount > 0 && index >= coins.size() )
+//             return 0;
+//         if(amount == 0 )
+//             return 1;
         
-        if(sum == amount)
-            return 1;
+//         if(dp[index][amount] != -1)
+//             return dp[index][amount];
+//         int takeIt = 0;
+//         if(amount-coins[index] >= 0)
+//         takeIt = findCombinations(index, amount-coins[index], coins, dp);
         
-        return func(i, sum+a[i], amount, a)+func(i+1, sum, amount, a);
-            
+//         int notTakeIt = findCombinations(index+1, amount, coins, dp);
         
-    }
+//         return dp[index][amount] = takeIt+notTakeIt;
+//     }
     int change(int amount, vector<int>& coins) {
+        // vector<vector<int>>dp(coins.size()+1, vector<int>(amount+1, -1));
+        // return findCombinations(0, amount, coins, dp);
         
-        // return func(0, 0, amount, coins);
-        int size = coins.size();
-        int dp[size+1][amount+1];
+        int dp[coins.size()+1][amount+1];
         
-        for(int i = 0; i <= size; i++) {
-         
-            dp[i][0] = 1;
-        }
-        for(int i = 0; i <= amount; i++) {
+        for(int i = 0; i <= amount; i++)
             dp[0][i] = 0;
-            // dp[i][0] = 1;
-        }
+        for(int i = 0; i <= coins.size(); i++)
+            dp[i][0] = 1;
         dp[0][0] = 1;
         
-        for(int i = 1; i <= size; i++) {
+        for(int i = 1; i <= coins.size(); i++) {
             for(int j = 1; j <= amount; j++) {
-                int sum = 0;
-                if(j-coins[i-1] >= 0)
-                    sum += dp[i][j-coins[i-1]];
-                sum += dp[i-1][j];
-                dp[i][j] = sum;
+                int takeIt = 0, notTakeIt = 0;
+                if(j-coins[i-1] >= 0) 
+                    takeIt += dp[i][j-coins[i-1]];
+                notTakeIt+= dp[i-1][j];
+                dp[i][j] = takeIt+notTakeIt;
             }
         }
         
-        return dp[size][amount];
+        return dp[coins.size()][amount];
         
     }
 };
