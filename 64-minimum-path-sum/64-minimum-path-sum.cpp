c953bb08950ee739af3b6d8aca4a896ffcde29ce
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-05-28 20:21:58 +0530

    Time: 19 ms (19.36%), Space: 9.8 MB (75.36%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -7,17 +7,17 @@ public:
         int dp[rows][columns];
         dp[0][0] = grid[0][0];
         for(int col = 1; col < columns; col ++)
-            dp[0][col] = grid[0][col] + dp[0][col-1];
+            grid[0][col] = grid[0][col] + grid[0][col-1];
         for(int row = 1; row < rows; row++) 
-            dp[row][0] = grid[row][0] + dp[row-1][0];
+            grid[row][0] = grid[row][0] + grid[row-1][0];
         
         for(int row = 1; row < rows; row++) {
             for(int col = 1; col < columns; col++) {
-                dp[row][col] = grid[row][col] + min(dp[row-1][col],dp[row][col-1]);
+                grid[row][col] = grid[row][col] + min(grid[row-1][col],grid[row][col-1]);
             }
         }
         
-        return dp[rows-1][columns-1];
+        return grid[rows-1][columns-1];
    
     }
 };
