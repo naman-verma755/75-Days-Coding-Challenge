8e403fd11d6221af85be641c568c9f5a40908cf5
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-05-31 10:29:06 +0530

    Time: 599 ms (5.01%), Space: 13.1 MB (64.41%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,37 +1,28 @@
 class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
+
         
-        map<int,int>mp;
-        for(auto it : nums) {
-            mp[it] ++;
-        }
+        map<int, int>mp;
+        for(int &num : nums) 
+            mp[num]++;
         vector<int>v;
         for(auto it : mp)
             v.push_back(it.first);
-        int ans[v.size()];
-        int maxi = 0;
-        for(int i = 0; i < v.size(); i++)
-        {
-            ans[i] = (v[i]*mp[v[i]]);
-            int a = 0;
-            for(int j = i-1; j >= 0; j--)
-            {
-                if(abs(v[i]-v[j])>1)
-                a = max(a, ans[j]);
+        vector<int>dp(v.size()+1);
+        int ans = 0;
+        for(int i = 0; i < v.size(); i++) {
+            int maxPoints = v[i] * mp[v[i]];
+            
+            int maxi = 0;
+            for(int j = 0; j < i; j++) {
+                if(v[i]-v[j] > 1)
+                    maxi= max(maxi, dp[j]);
             }
-            ans[i] += a;
-            maxi = max(maxi, ans[i]);
+            dp[i] = maxPoints + maxi;
+            ans = max(ans, dp[i]);
         }
-        return maxi;
-        // prev = -1;
-        // int ans[i]
-        // for(int i =0; i<v.size();i++)
-        //     cout<<ans[i]<<" ";
-        // if(v.size() < 2)
-        //     return ans[0];
-        // if(v.size() <= 2)
-        //     return max(ans[0], ans[1]);
-        // return max(ans[v.size()-1],max( ans[v.size()-2], ans[v.size()-3]));
+        
+        return ans;
     }
 };
