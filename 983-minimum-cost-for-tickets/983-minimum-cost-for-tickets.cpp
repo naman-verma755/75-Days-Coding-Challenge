c835e19aa1715a60f678eea8712c9265e8f74496
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-05-31 11:10:14 +0530

    Time: 10 ms (26.60%), Space: 10.1 MB (20.95%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
@@ -1,54 +1,29 @@
 class Solution {
 public:
-    int findMin(int maxi, vector<int>&days, vector<int>&costs, map<int, int>&mp) {
+   int func(int day, set<int>&d, vector<int>&costs, vector<int>&dp) {
         
-          if(maxi < 0)
-              return 0;
+        if(day <= 0)
+            return 0;
+       if(dp[day] != -1)
+           return dp[day];
         
-          if(!mp.count(maxi)) {
-              return findMin(maxi-1, days, costs, mp);
-          } else {
-              
-              int a = findMin(maxi-1, days, costs, mp)+costs[0];
-              int b = findMin(maxi-7, days, costs, mp)+costs[1];
-              int c = findMin(maxi-30, days, costs, mp)+costs[2];
-              
-              return min(a, min(b,c));
-          }
+        if(d.find(day) != d.end()) {
+            
+            int r1 = func(day-1,d, costs, dp)+costs[0];
+            int r2 = func(day-7, d, costs, dp)+costs[1];
+            int r3 = func(day-30, d, costs, dp)+costs[2];
+            
+            return dp[day] = min(r1, min(r2, r3));
+        }
+        else
+            return dp[day] = func(day-1, d, costs, dp);
     }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        
-        int maxi = 0;
-        map<int, int>mp;
-        for(int it : days) {
-            maxi = max(it, maxi);
-            mp[it] ++;
-        }
-        vector<int>dp(maxi+2, 0);
-        
-        for(int i = 1; i <= maxi; i++) {
-            
-            if(mp.count(i)) {
-                // cout<<i<<" ";
-                int mini = INT_MAX;
-               int a = costs[0], b = costs[1], c = costs[2];
-                if(i-1 >= 0) 
-                    a += dp[i-1];
-                if(i-7 >= 0)
-                    b += dp[i-7];
-                if(i-30 >= 0)
-                    c += dp[i-30];
-                // if(mini != INT_MAX)
-                dp[i] = min(a, min(b,c));
-            }
-            else
-                dp[i] = dp[i-1];
-        }
-        // for(auto it : dp)
-        //     cout<<it<<" ";
-        return dp[maxi];
-        // return findMin(maxi, days, costs, mp);
-        
-        
+        set<int>st;
+        for(int day : days)
+            st.insert(day);
+        int maxDay = days[days.size()-1];
+        vector<int>dp(maxDay+1, -1);
+        return func(maxDay, st, costs, dp);
     }
 };
