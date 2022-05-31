d4ce26d89deb547a375972ed1b66d43c710947f8
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-05-31 09:37:19 +0530

    Time: 12 ms (6.38%), Space: 7.5 MB (13.43%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,50 +1,30 @@
 class Solution {
 public:
-     
-//     int findWays(int index, string s,  vector<int>&dp ){
-//         if(index == s.length())
-//             return 1;
+    int func(int index, string s, vector<int>&dp) {
         
-//         if(s[index] == '0')
-//             return 0;
+        int len = s.length();   // string length
+        if(index == len)
+            return 1;
         
-//          if(dp[index] != -1)
-//              return dp[index];
-//         int count = 0;
-//         count += findWays(index+1, s, dp);
-//         string res = "";
-//         if(index+1 < s.length()) {
-//         res = res+s[index]+s[index+1];
-//         if(res >= "10" && res <= "26")
-//             count += findWays(index+2, s, dp);
-//         }
+        if(s[index] == '0')
+            return 0;
+        if(dp[index] != -1)
+            return dp[index];
+        int count = 0;
         
-//         return dp[index] = count;
-        
-
-//     }
-    int numDecodings(string s) {
-        int dp[s.length()+1];
-        dp[0] = 1;
-        if(s[0] != '0')
-            dp[1] = 1;
-        else
-            dp[1] = 0;
-        
-        for(int i = 2; i <= s.length(); i++) {
-            int count = 0;
-            if(s[i-1] != '0') {
-                count += dp[i-1];
-            }
-          
-            string res = "";res = res+s[i-2]+s[i-1];
-            if(res >= "10" && res <= "26") {
-                count += dp[i-2];
-            }
-            
-            dp[i] = count;
+        count += func(index+1, s, dp);
+        string ans = "";
+        ans += s[index];
+        if(index+1 < len)
+        {
+            ans += s[index+1];
+            if(ans >= "10" && ans <= "26")
+            count += func(index+2, s, dp);
         }
-        return dp[s.length()];
-        // return findWays(0, s, dp);
+        return dp[index] = count;
+    }
+    int numDecodings(string s) {
+        vector<int>dp(s.length(), -1);
+        return func(0, s, dp);
     }
 };
