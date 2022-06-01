3a8fadbee03b8870377683d9409637fb57aead97
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-06-01 22:54:10 +0530

    Time: 55 ms (69.99%), Space: 11.2 MB (56.30%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -1,34 +1,58 @@
 class Solution {
 public:
-    bool isMatch(string str,string pattern) {
+    bool func(int i, int j, string p, string s, vector<vector<int>>&dp) {
         
-        
-       int pLength = pattern.length();
-       int strLength = str.length();
-        
-        bool dp[pLength+1][strLength+1];
-        memset(dp, false, sizeof(dp));
-        dp[0][0] = true;
-        
-        for(int i = 1; i <= pLength; i++) {
-            if(pattern[i-1] == '*')
-            dp[i][0] = true;
-            else
-            break;
-        }
-        for(int i = 1; i <= pLength; i++) {
-            for(int j = 1; j <= strLength; j++) {
-                if(pattern[i-1] == str[j-1] || pattern[i-1] == '?')
-                dp[i][j] = dp[i-1][j-1];
-                else if(pattern[i-1] == '*') 
-                dp[i][j] = dp[i-1][j] || dp[i][j-1];
-                else
-                dp[i][j] = false;
+        if(i == p.length() && j == s.length())
+            return true;
+        if(j < s.length() && i == p.length())
+            return false;
+        if(j == s.length()) {
+            for(int it = i; it < p.length(); it++) {
+                if(p[it] != '*')
+                    return false;
             }
+            return true;
         }
-        return dp[pLength][strLength];
-        // return findMatch(0, 0, pattern, str);
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        if(p[i] == s[j] || p[i] == '?') {
+            return dp[i][j] = func(i+1, j+1, p, s, dp);
+        }
+        else if(p[i] == '*') 
+            return dp[i][j] = (func(i, j+1, p, s, dp) || func(i+1, j, p, s, dp));
+        else
+            return dp[i][j] = false;
+        
         
     }
-    
+    bool isMatch(string s, string p) {
+        // vector<vector<int>>dp(p.length(), vector<int>(s.length(), -1));
+        
+        int dp[p.length()+1][s.length()+1];
+        for(int i = 0; i <= s.length(); i++) 
+            dp[0][i] = false;
+        dp[0][0] = true;
+        int i = 1;
+        for( ; i <= p.length(); i++) {
+            if(p[i-1] != '*')
+                break;
+            dp[i][0] = true;
+        }
+            
+        for(; i<= p.length() ;i++) 
+            dp[i][0] = false;
+        
+        for(int i = 1; i <= p.length(); i++) {
+            for(int j = 1; j <= s.length(); j++) {
+                if(p[i-1] == s[j-1] || p[i-1] == '?')
+                    dp[i][j] = dp[i-1][j-1];
+                else if(p[i-1] == '*')
+                    dp[i][j] = dp[i][j-1] || dp[i-1][j];
+                else
+                    dp[i][j] = false;
+            }
+        }
+        return dp[p.length()][s.length()];
+        // return func(0, 0, p, s, dp);
+    }
 };
