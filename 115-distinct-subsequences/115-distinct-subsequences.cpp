bc83342204943f34dddbb5cee9e0f8be34ae300b
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-06-01 21:54:08 +0530

    Time: 19 ms (93.71%), Space: 10.8 MB (77.31%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,29 +1,42 @@
 class Solution {
 public:
-    
-    
-    int numDistinct(string s, string t) {
-       
-        int mod = 1000000007;
-        int size1 = s.length();
-        int size2 = t.length();
-        vector<int>prev(size2+1,0);
-        prev[0] = 1;
-        vector<int>curr(size2+1,0);
+    int func(int i, int j, string s, string t, vector<vector<int>>&dp) {
         
-        for(int i = 1; i <= size1; i++) {
-            curr[0] = 1;
-            for(int j = 1; j <= size2; j++) {
-                int a = 0;
-                if(s[i-1] == t[j-1]) {
-                    a = prev[j-1];
-                }
-                int b = prev[j];
-                curr[j] = (a+b)%mod;
-            }
-            prev = curr;
+        
+        if(j == t.length())
+            return 1;
+        if(i == s.length())
+            return 0;
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        if(s[i] == t[j]) {
+            
+            int c1 = func(i+1, j+1, s, t, dp);
+            int c2 = func(i+1, j, s, t, dp);
+            return dp[i][j] = c1+c2;
         }
-        
-        return prev[size2];
+        else
+            return dp[i][j] = func(i+1, j, s, t, dp);
+    }
+    int numDistinct(string s, string t) {
+        // vector<vector<int>>dp(s.length(), vector<int>(t.length(), -1));
+        int mod = 1000000007;
+        int slen = s.length(), tlen = t.length();
+        int dp[slen+1][tlen+1]; 
+        for(int i = 0; i <= tlen; i++)
+            dp[0][i] = 0;
+        for(int i = 0; i <= slen; i++)
+            dp[i][0] = 1;
+        for(int i = 1; i <= slen; i++) {
+            for(int j = 1; j <= tlen; j++) {
+                if(s[i-1] == t[j-1]) {
+                    dp[i][j] = (dp[i-1][j-1] + dp[i-1][j])%mod;
+                }
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[slen][tlen];
+        // return func(0, 0, s, t, dp);
     }
 };
