517979aefbb6ca06faebd2b4c8487486ea23a5d1
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-06-11 16:08:34 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -9,79 +9,68 @@ class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
-	
 	int find(int u, int parent[]) {
 	    if(u == parent[u])
 	    return u;
 	    
-	    return parent[u] = find(parent[u], parent);
+	    return parent[u] = find(parent[u], parent); // path compression;
 	}
-	void unionOf(int u, int v, int parent[], int rank[]) {
+	
+	void unionOf(int u, int v, int rank[], int parent[]) {
 	    
-	    if(rank[u] < rank[v]) 
-	    parent[u] = parent[v];
-	    else if(rank[u] > rank[v])
-	    parent[v] = u;
-	    else
-	    {
-	        rank[u] ++;
+	    if(rank[u] < rank[v]) {
+	        parent[u] = v;
+	    }
+	    else if(rank[v] < rank[u]) {
+	        parent[v] = u;
+	    }
+	    else {
+	        rank[u]++;
 	        parent[v] = u;
 	    }
 	}
     int spanningTree(int V, vector<vector<int>> adj[])
     {
         // code here
-       priority_queue<pair<int, pair< int, int>>, vector<pair<int, pair< int, int>>>, greater<pair<int, pair< int, int>>>>pq;
-       
-       for(int i = 0; i < V; i++) {
-           for(auto it : adj[i]) {
-               int wt = it[1];
-               int v = it[0];
-               pq.push({wt, {i, v}});
-           }
-       }
-       
-       int parent[V];
-       int rank[V];
-       for(int i = 0; i < V; i++) {
-           parent[i] = i;
-           rank[i] = 0;
-       }
-       int sum = 0;
-       while(!pq.empty()) {
-           
-           auto it = pq.top();
-           pq.pop();
-           int wt = it.first;
-           int u = it.second.first;
-           int v = it.second.second;
-           
-           int parentu = find(u, parent);
-           int parentv = find(v, parent);
-           
-           if(parentu == parentv)
-           continue;
-           
-           sum += wt;
-           unionOf(parentu, parentv, parent, rank);
-       }
+        int parent[V];
+        int rank[V];
+        for(int i = 0; i < V; i++) {
+            parent[i] = i;
+            rank[i] = 0;
+        }
+        vector<vector<int>>v;
         
+        for(int i = 0; i < V; i++) {
+            for(vector<int>&edge : adj[i]) 
+            v.push_back(vector<int>()= {edge[1], i, edge[0]});
+        }
         
-        
-       return sum;
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        sort(v.begin(), v.end());
+        int count = 0, cost = 0;
+        int i = 0;
+        // cout<<v[0][0]<<v[0][1]<<v[0][2];
+        while(i < v.size()) {
+            // cout<<v[i][1]<<" "<<v[i][2]<<endl;
+            int wt = v[i][0];
+            int u = v[i][1];
+            int e = v[i][2];
+            
+            int parentU = find(u, parent);
+            int parentV = find(e, parent);
+            
+            if(parentU == parentV) {
+                i++;
+                continue;
+            }
+            
+            unionOf(parentU, parentV, rank, parent);
+            
+            count++;
+            cost += wt;
+            i++;
+            
+        }
+        return cost;
     }
 };
 
