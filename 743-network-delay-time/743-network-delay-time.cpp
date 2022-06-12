d7688550c663da67e19879c0ef4153b850a3257b
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-06-12 13:24:01 +0530

    Time: 169 ms (53.44%), Space: 40 MB (59.84%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
-    void buildGraph(vector<pair<int, int>>adj[], vector<vector<int>>&times) {
+    void buildGraph(vector<vector<int>>& times, vector<pair<int, int>>adj[]) {
         
-        for(auto it : times) {
-            adj[it[0]].push_back({it[1], it[2]});
+        for(vector<int>&edge: times) {
+            int u = edge[0];
+            int v = edge[1];
+            int wt = edge[2];
+            
+            adj[u].push_back({v, wt});
         }
     }
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         
         vector<pair<int, int>>adj[n+1];
-        buildGraph(adj, times);
+        buildGraph(times, adj);
+        
         int dist[n+1];
         
         for(int i = 1; i <= n; i++)
             dist[i] = INT_MAX;
-        
         
         set<pair<int, int>>st;
         
@@ -23,27 +27,30 @@ public:
         
         while(!st.empty()) {
             
-            auto it = *(st.begin());
-            st.erase(it);
-            int u = it.second;
-            int wt1 = it.first;
+            pair<int, int>temp = *(st.begin());
+            st.erase(temp);
+            int u = temp.second;
+            int wt1 = temp.first;
             
-            for(auto itr : adj[u]) {
-                int v = itr.first, wt2 = itr.second;
-                
+            for(auto it: adj[u]) {
+                int v = it.first, wt2 = it.second;
                 if((wt1+wt2) < dist[v]) {
-                    if(dist[v] != INT_MAX) 
+                    
+                    if(dist[v] != INT_MAX)
                         st.erase({dist[v], v});
                     dist[v] = (wt1+wt2);
                     st.insert({dist[v], v});
                 }
             }
         }
-        int maxi = -1;
-        for(int i = 1; i <=n; i++)
-            maxi = max(dist[i], maxi);
-        if(maxi == INT_MAX)
+        
+        int minTime = -1;
+        for(int i = 1; i <= n; i++)
+            minTime = max(minTime, dist[i]);
+        
+        if(minTime == INT_MAX)
             return -1;
-        return maxi;
+        return minTime;
+        
     }
 };
