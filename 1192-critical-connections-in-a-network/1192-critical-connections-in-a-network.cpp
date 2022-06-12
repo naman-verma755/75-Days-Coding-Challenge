b94a43b46caa8e6640e5ca277f55efc3cee2aa89
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-06-12 16:29:21 +0530

    Time: 1154 ms (22.65%), Space: 167.5 MB (85.41%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -1,49 +1,48 @@
 class Solution {
 public:
-    vector<vector<int>>ans; 
-    void dfs(int node, int parent, int &counter, int low[], int tin[], vector<int>adj[], vector<bool>&visited) {
+    void buildGraph(vector<int>adj[],  vector<vector<int>>& connections) {
         
-        visited[node] = true;
+        for(vector<int>&edge: connections) {
+            int u = edge[0], v = edge[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+    }
+    void func(int node, int parent, int tin[], int low[], vector<vector<int>>&ans, int &counter, vector<int>adj[]) {
+        
+        
         low[node] = counter;
         tin[node] = counter;
         counter++;
-        for(int vertex : adj[node]) {
-            if(!visited[vertex]) {
-                dfs(vertex, node, counter, low, tin, adj, visited);
-                // if(node == 0 && vertex == 1) {
-                //     cout<<low[vertex]<<" "<<tin[vertex]<<endl;
-                // }
-                if(low[vertex] > tin[node]) {
-                    vector<int>k = {node, vertex};
-                    ans.push_back(k);
-                }
-                else {
-                    low[node] = min(low[node], low[vertex]);
-                }
-                // if(node == 0 && vertex == 1) {
-                //     cout<<low[vertex]<<" "<<tin[vertex]<<endl;
-                // }
-            
-            } else if(vertex != parent) {
-                low[node] = min(low[node], low[vertex]);
-            }
-        }
         
+        for(auto it : adj[node]) {
+            
+            if(it == parent)
+                continue;
+            if(tin[it] == -1)
+                func(it, node, tin, low, ans, counter, adj);
+            low[node] = min(low[it], low[node]);
+            
+            if(low[it] > tin[node])
+            {
+                ans.push_back(vector<int>()={node, it});
+            }
+            
+            
+        }
     }
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
-        
+     
         vector<int>adj[n];
-        for(vector<int>&edge : connections) {
-            adj[edge[0]].push_back(edge[1]);
-            adj[edge[1]].push_back(edge[0]);
-        }
-        int low[n];
-        int tin[n];
-        int counter = 1;
-        
-        vector<bool>visited(n, false);
-        dfs(0, -1, counter, low, tin, adj, visited);
-        
+        buildGraph(adj, connections);
+        int tin[n];  //keeping time of insertion
+        int low[n];  //keeping lowest time of insertion
+        int counter = 0;
+        for(int i = 0; i < n; i++)
+        tin[i] = low[i] = -1;
+        vector<vector<int>>ans;
+        func(0, -1, tin, low, ans, counter, adj);
         return ans;
+        
     }
 };
