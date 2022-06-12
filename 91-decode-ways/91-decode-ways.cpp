1348caf075b5aa6264d76a2586a29d4db1d9227a
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-06-12 17:41:37 +0530

    Time: 8 ms (13.53%), Space: 6.2 MB (44.23%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -21,8 +21,26 @@ public:
     }
     int numDecodings(string s) {
         
-        vector<int>dp(s.length(), -1);
-        return decode(0, s, dp);
+        vector<int>dp(s.length()+1);
+        if(s[0] == '0')
+            return 0;
+        
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i <= s.length(); i++) {
+            int way1 = 0, way2 = 0;
+            string ans = "";
+            ans += s[i-1];
+            if(ans != "0")
+                way1 = dp[i-1];
+            ans = s[i-2]+ans;
+            if(ans >= "1" && ans <= "26")
+                way2 = dp[i-2];
+            
+            dp[i] = way1+way2;
+        }
+        return dp[s.length()];
+//         return decode(0, s, dp);
         
     }
 };
