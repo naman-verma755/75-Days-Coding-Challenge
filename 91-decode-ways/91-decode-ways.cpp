d5d8bfe3599d5d55e5d682a64d44d402ae10c717
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-06-12 17:15:07 +0530

    Time: 11 ms (8.20%), Space: 7.4 MB (16.11%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,46 +1,28 @@
 class Solution {
 public:
-    int func(int index, string s, vector<int>&dp) {
+    int decode(int index, string s, vector<int>&dp) {
         
-        int len = s.length();   // string length
-        if(index == len)
+        if(index == s.length())
             return 1;
-        
-        if(s[index] == '0')
+        string ans = "";
+        ans += s[index];
+        if(ans == "0")
             return 0;
         if(dp[index] != -1)
             return dp[index];
-        int count = 0;
-        
-        count += func(index+1, s, dp);
-        string ans = "";
-        ans += s[index];
-        if(index+1 < len)
-        {
+        int way1 = decode(index+1, s, dp), way2 = 0;
+        if(index+1 < s.length()) {
             ans += s[index+1];
-            if(ans >= "10" && ans <= "26")
-            count += func(index+2, s, dp);
+            if(ans >= "1" && ans <= "26") 
+            way2 = decode(index+2, s, dp);
         }
-        return dp[index] = count;
+        return dp[index] = way1+way2;
+        
     }
     int numDecodings(string s) {
-        vector<int>dp(s.length()+1, 0);
-        dp[0] = 1;
-        for(int i = 1; i <= s.length(); i++) {
-            int count = 0;
-            if(s[i-1] != '0') {
-                count += dp[i-1];
-            }
-            string ans = "";
-            ans += s[i-1];
-            if(i-2 >= 0) {
-                ans = s[i-2]+ans;
-                if(ans >= "1" && ans <= "26") 
-                    count += dp[i-2];
-            }
-            dp[i] = count;
-        }
-        return dp[s.length()];
-        // return func(0, s, dp);
+        
+        vector<int>dp(s.length(), -1);
+        return decode(0, s, dp);
+        
     }
 };
