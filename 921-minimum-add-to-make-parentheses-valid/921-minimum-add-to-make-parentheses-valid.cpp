06568bfab1ab4062c02f2dd3a561d401c7bf05c4
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-06-13 20:08:56 +0530

    Time: 3 ms (45.52%), Space: 6.4 MB (23.21%) - LeetHub

---
+++ 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        
+        stack<char>st;
+        
+        for(int i = 0; i < s.length(); i++) {
+            
+            
+            if(!st.empty() && st.top() == '(' && s[i] == ')')
+            {
+                st.pop();
+                continue;
+            }
+            
+            st.push(s[i]);
+        }
+        return st.size();
+    }
+};
