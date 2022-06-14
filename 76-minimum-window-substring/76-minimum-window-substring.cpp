527c31e455c50bdc8f791c3ee6422d02c52e8160
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-06-14 10:43:54 +0530

    Time: 47 ms (28.45%), Space: 8.3 MB (29.09%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -2,43 +2,52 @@ class Solution {
 public:
     string minWindow(string s, string t) {
         
-        map<char, int>mp1, mp2;
+        
         int count = 0;
-        for(char ch : t)
-            mp1[ch]++;
-        
-        int left = 0, right = 0;
-        int maxi = INT_MAX;
+        map<char, int>mp, mp2;
         string ans = "";
-        while(right < s.length()) {
+        int maxLength = INT_MAX;
+        for(char ch: t)
+            mp[ch]++;
+        int left = 0;
+        for(int right = 0; right < s.length(); right++) {
             
-            mp2[s[right]] ++;
-            // cout<<s[right]<<" "<<mp2[s[right]]<<endl;
-            
-            if(mp1.count(s[right]) && mp2[s[right]] <= mp1[s[right]])
+            mp2[s[right]]++;
+            if(mp.count(s[right]) && mp2[s[right]] <= mp[s[right]])
                 count++;
-           
+            
             if(count == t.length()) {
-                // cout<<"Yes\n";
-                // cout<<s[left]<<" "<<mp1[s[left]]<<" "<<mp2[s[left]]<<" "<<count<<endl;
-            while(mp1[s[left]] < mp2[s[left]] || !mp1.count(s[left])) {
-                // cout<<right<<" "<<s.substr(left, (right-left+1))<<"\n";
                 
-                if( mp1[s[left]] < mp2[s[left]])
-                mp2[s[left]]--;
+                while(!mp.count(s[left]) || mp2[s[left]] > mp[s[left]]) {
+                    if(mp2[s[left]] > mp[s[left]])
+                    mp2[s[left]]--;
                 left++;
-            }
-            int length = right-left+1;
-            if(maxi > length) {
-                    
-                    maxi = length;
-                    ans = s.substr(left, length);
                 }
-                
+                  int len = right-left+1;
+                if(len < maxLength) {
+                    maxLength = len;
+                ans = s.substr(left, len);
+                }
             }
-            right++;
+//             while(left < right && (count > t.length() || !mp.count(s[left])))
+//             {
+//                 if(mp.count(s[left]))
+//                     count--;
+                
+//                 left++;
+//             }
+//             while(count > t.length()) {
+                
+//                 if(mp.count(s[left]))
+//                     count--;
+//                 left++;
+//             }
+            
+            // if(count == t.length())
+          
+            // cout<<ans<<" ";
         }
-        
+         
         return ans;
     }
 };
