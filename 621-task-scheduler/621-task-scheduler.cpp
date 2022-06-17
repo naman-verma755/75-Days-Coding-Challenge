39173f644c2b5fef1a916a264467080f993ec76d
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-06-17 14:11:55 +0530

    Time: 114 ms (57.08%), Space: 34.6 MB (47.46%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.cpp
+++ 621-task-scheduler/621-task-scheduler.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        
-        int maxFreq = 0;
+        if(n == 0)
+            return tasks.size();
         map<char, int>mp;
-        for(char &ch : tasks) {
+        int maxi = 0;
+        for(char ch: tasks) 
+        {
             mp[ch] ++;
-            maxFreq = max(maxFreq, mp[ch]);
+            maxi = max(maxi, mp[ch]);
         }
         
-        int ans = (maxFreq-1)*(n+1);
-        for(auto it : mp) {
-            if(it.second == maxFreq)
+        int ans = ((maxi-1) * (n+1));
+        
+        for(auto it : mp)
+            if(it.second == maxi)
                 ans++;
-        }
         
-       
         return max((int)tasks.size(), ans);
     }
 };
