762f730c9d7ec1167cf4bd5b6ae11f4acfdccf40
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-06-21 09:58:27 +0530

    Time: 186 ms (42.81%), Space: 10.8 MB (78.25%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
@@ -1,28 +1,41 @@
 class Solution {
 public:
+    int func(int start, int end, string s, vector<vector<int>>&dp) {
+        
+        if(start == end)
+            return 1;
+        
+        if(start > end)
+            return 0;
+        
+        if(dp[start][end] != -1)
+            return dp[start][end];
+            
+        if(s[start] == s[end])
+            return dp[start][end] = func(start+1, end-1, s, dp) + 2;
+        else {
+            return dp[start][end] = max(func(start+1, end, s, dp), func(start, end-1, s, dp));
+        }
+        return 0;
+    }
     int longestPalindromeSubseq(string s) {
         
-        int length = s.length();
-        int dp[length+1][length+1];
+        // vector<vector<int>>dp(s.length()+1, vector<int>(s.length()+1, -1))
+        int len = s.length();
+        int dp[len+1][len+1];
         
-        for(int i = 0; i <= length; i++) {
-            dp[0][i] = 0;
-            dp[i][length] = 0;
-        }
+        for(int i = 0; i <= len; i++) 
+            dp[0][i] = 0,dp[i][len] = 0 ;
         
-        for(int i = 1; i <= length; i++) {
-            for(int j = length-1; j >= 0; j--) {
-                
-                if(s[i-1] == s[j]) {
-                    dp[i][j] = dp[i-1][j+1]+1;
-                }
+        for(int i = 1; i <= len; i++) {
+            for(int j = len-1; j >= 0; j--) {
+                if(s[i-1] == s[j])
+                    dp[i][j] = dp[i-1][j+1] + 1;
                 else
                     dp[i][j] = max(dp[i-1][j], dp[i][j+1]);
             }
         }
-       
-        
-        return dp[length][0];
-        
+        return dp[len][0];
+        // return func(0, s.length()-1, s, dp);
     }
 };
