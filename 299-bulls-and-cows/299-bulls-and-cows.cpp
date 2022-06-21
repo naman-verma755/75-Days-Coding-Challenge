ee394ff72281717e3022e3a8eff7a2e20e143357
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-06-21 22:01:44 +0530

    Time: 4 ms (76.08%), Space: 6.6 MB (43.60%) - LeetHub

--- 299-bulls-and-cows/299-bulls-and-cows.cpp
+++ 299-bulls-and-cows/299-bulls-and-cows.cpp
@@ -2,7 +2,9 @@ class Solution {
 public:
     string getHint(string secret, string guess) {
         
-        map<char, int>mp;
+        int mp1[10] = {};
+        int mp2[10] = {};
+    
         int len = secret.length();
         int bulls = 0, cows = 0;
         for(int i = 0; i < len; i++) {
@@ -11,17 +13,26 @@ public:
                 bulls++;
             }
             else {
-                mp[secret[i]]++;
+                // cout<<secret[i]-'0'<<" ";
+                bool flag1 = false, flag2 = false;
+                if(mp2[secret[i]-'0'] > 0)
+                    cows++,flag1 = true,mp2[secret[i]-'0']--;
+                if(mp1[guess[i]-'0'] > 0)
+                    cows++,flag2 = true,mp1[guess[i]-'0']--;
+                if(!flag1)
+                mp1[secret[i]-'0']++;
+                if(!flag2)
+                mp2[guess[i]-'0']++;
             }
         }
         
-        for(int i = 0; i < len; i++) {
+//         for(int i = 0; i < len; i++) {
             
-            if(guess[i] != -1 && mp.count(guess[i]) && mp[guess[i]] > 0) {
-                mp[guess[i]]--;
-                cows++;
-            }
-        }
+//             if(guess[i] != -1 && mp.count(guess[i]) && mp[guess[i]] > 0) {
+//                 mp[guess[i]]--;
+//                 cows++;
+//             }
+//         }
         
         return (to_string(bulls)+"A"+to_string(cows)+"B");
         
