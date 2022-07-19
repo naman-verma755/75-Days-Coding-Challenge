273889a63c709a90c5e74b55917d7f417346b360
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-07-19 15:26:49 +0530

    Time: 191 ms (5.00%), Space: 63.1 MB (8.87%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,73 +1,70 @@
-class priorityQueue {
+class pqueue{
     public:
-    int a[1000000];
+    int a[100001];
     int size = 0;
     
-    void heapify(int);
-    void buildHeap(vector<int>&v, int k);
-    int top() {
-        return a[0];
-    }
-    
+    void push(int key);
+    void maxHeapify( int index);
+    int top();
     void pop();
-    void push(int element);
-    
+    void print() {
+        for(int j = 0; j < size; j++)
+            cout<<a[j]<<" ";
+        cout<<endl;
+    }
 };
-int parent(int i) { return (i-1)/2; }
-void priorityQueue::pop() {
-   a[0] = a[size-1];
-        size--;
-       heapify(0);
-}
-void priorityQueue::push(int element) {
-    a[size] = element;
-    size++;
-    int i = size-1;
-    while(i > 0 && a[(i-1)/2] > a[i])
-    {
-        swap(a[(i-1)/2], a[i]);
-        i = (i-1)/2;
-    }
-}
- void priorityQueue::buildHeap(vector<int>&v, int k) {
-        for(int i = 0; i < k; i++)
-        {
-            a[i] = v[i];
-            size++;
-        }
-        for(int i = size-1; i >= 0; i --) {
-            heapify(i);
-        }
-    }
 
-void priorityQueue::heapify(int index) {
-    int left = 2*index+1;
-    int right = 2*index+2;
-    int mini = index;
-    if(left < size && a[left] < a[mini])
-        mini = left;
-    if(right < size && a[right] < a[mini])
-        mini = right;
-    if(mini != index)
-    {
-        swap(a[mini], a[index]);
-        heapify( mini);
+void pqueue::push(int element) {
+    int index = size++;
+    a[index] = element;
+    int parent = (index-1)/2;
+    while(index > 0 && a[parent] > a[index]) {
+        swap(a[parent], a[index]);
+        index = parent;
+        parent = (index-1)/2;
     }
 }
+void pqueue::maxHeapify( int index) {
+  
+    int left = (2*index+1);
+    int right = (2*index+2);
+    int largest = index;
+    if(left < size && a[left] < a[largest]) 
+        largest = left;
+    if(right < size && a[right] < a[largest])
+        largest = right;
+    
+    if(largest != index) {
+        swap(a[index], a[largest]);
+        maxHeapify(largest);
+    }
+}
+
+void pqueue::pop() {
+   
+    a[0] = a[size-1];
+    size--;
+    maxHeapify(0);
+}
+int pqueue::top() {
+  return a[0];
+}
+
 
 class Solution {
 public:
-    
     int findKthLargest(vector<int>& nums, int k) {
-        priorityQueue pq;
-        pq.buildHeap(nums, k);
-        int i = k;
-        for(; i < nums.size(); i++) {
-            pq.push(nums[i]);
+        
+        int n = nums.size();
+       pqueue *pq = new pqueue();
+        for(int  i = 0; i < n; i++) {
+            pq->push(nums[i]);
+            // pq->print();
+            if(pq->size > k)
+                pq->pop();
             
-            if(pq.size > k)
-                pq.pop();
         }
-        return pq.top();
+     return pq->top();   
     }
+    
 };
