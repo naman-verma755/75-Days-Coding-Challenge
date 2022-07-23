7f5272e011cb29df8804f66c50074766bf2fe8c8
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-07-23 10:59:34 +0530

    Time: 3 ms (54.07%), Space: 7.6 MB (75.68%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,19 +1,35 @@
 class Solution {
 public:
+    
+//     int func(int index, vector<int>&nums) {
+//         if(index >= nums.size())
+//             return 0;
+        
+//         int money1 = func(index+2, nums);
+//         int money2 = func(index+3, nums);
+        
+//         return max(money1, money2) + nums[index];
+//     }
     int rob(vector<int>& nums) {
+        
         int size = nums.size();
+        int dp[size];
+        if(size == 2)
+            return max(nums[0], nums[1]);
         if(size == 1)
             return nums[0];
-        for(int i = 0; i < size; i++) {
-            int k = -1;
-            if(i-2 >= 0)
-                k = nums[i-2] + nums[i];
-            if(i-3 >= 0)
-                k = max(k,(nums[i-3] + nums[i]));
-            if(k != -1)
-            nums[i] = k;
-        }
-        return max(nums[size-1], nums[size-2]);
+
         
+        dp[0] = nums[0];
+        dp[1] = nums[1];
+        dp[2] = nums[2]+nums[0];
+        // if(size <= 2)
+            
+        for(int i = 3; i < size; i++) {
+            
+            dp[i] = nums[i] + max(dp[i-2], dp[i-3]);
+        }
+        
+        return max(dp[size-1], dp[size-2]);
     }
 };
