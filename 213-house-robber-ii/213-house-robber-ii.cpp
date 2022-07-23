230794f5c4914c5e10271f6e956afeeeaa2e5dd7
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-07-23 12:04:14 +0530

    Time: 4 ms (38.68%), Space: 7.8 MB (61.89%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -4,49 +4,38 @@ public:
         
         int size = nums.size();
         if(size == 1)
-            return nums[0];
+            return nums[size-1];
         if(size == 2)
-            return max(nums[0], nums[1]);
-        int maxAmount1[size];
-        int maxAmount2[size];
-        maxAmount1[0] = nums[0];
-        maxAmount1[1] = nums[1];
+            return max(nums[size-1], nums[size-2]);
+        
+        if(size == 3)
+            return max(nums[0], max(nums[1], nums[2]));
+            
+        int dp1[size];
+        
         for(int i = 0; i < size-1; i++) {
-            int k = -1;
-            if(i-2 >= 0) 
-                k = nums[i] + maxAmount1[i-2];
+            
+            dp1[i] = nums[i];
             if(i-3 >= 0)
-                k = max(k,(nums[i] + maxAmount1[i-3]));
-            if(k != -1)
-                maxAmount1[i] = k;
-            else
-                maxAmount1[i] = nums[i];
-            
+                dp1[i] += max(dp1[i-2], dp1[i-3]);
+            else if(i-2 >= 0)
+                dp1[i] += dp1[i-2];
         }
-        int ans1 = max(maxAmount1[size-2], maxAmount1[size-3]);
-        maxAmount1[size-1] = nums[size-1];
-        maxAmount1[size-2] = nums[size-2];
+        
+        int dp2[size+1];
+        
         for(int i = size-1; i > 0; i--) {
-            int k = -1;
-            if(i+2 < size) 
-                k = nums[i] + maxAmount2[i+2];
-            if(i+3 < size)
-                k = max(k,(nums[i] + maxAmount2[i+3]));
-            if(k != -1)
-                maxAmount2[i] = k;
-            else
-                maxAmount2[i] = nums[i];
+            dp2[i] = nums[i];
             
+            if(i+3 < size)
+                dp2[i] += max(dp2[i+3], dp2[i+2]);
+            else if(i+2 < size)
+                dp2[i] += dp2[i+2];
         }
-        int ans2 = max(maxAmount2[1], maxAmount2[2]);
-//         for(int i =0 ; i< size-1; i++)
-//          cout<<maxAmount1[i]<<" ";
-//         cout<<endl;
-//          for(int i =1 ; i< size; i++)
-//          cout<<maxAmount2[i]<<" ";
         
+        int Money1 = max(dp1[size-2], dp1[size-3]);  // money amount gathered (exluding the last house)
+        int Money2 = max(dp2[1], dp2[2]);
         
-        return max(ans1, ans2);
-        
+        return max(Money1, Money2);
     }
 };
