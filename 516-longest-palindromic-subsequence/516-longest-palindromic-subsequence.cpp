1095fa9d5de05131c53063347349b372a433ecd3
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-07-23 15:36:21 +0530

    Time: 119 ms (73.28%), Space: 10.7 MB (88.52%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
@@ -1,41 +1,51 @@
 class Solution {
 public:
-    int func(int start, int end, string s, vector<vector<int>>&dp) {
+    int func(int left, int right, string s, vector<vector<int>>&dp) {
         
-        if(start == end)
+        if(left == right && s[left] == s[right])
             return 1;
-        
-        if(start > end)
+        if(left > right )
             return 0;
-        
-        if(dp[start][end] != -1)
-            return dp[start][end];
-            
-        if(s[start] == s[end])
-            return dp[start][end] = func(start+1, end-1, s, dp) + 2;
+        if(dp[left][right] != -1)
+            return dp[left][right];
+        if(s[left] == s[right]) 
+            return dp[left][right] =  func(left+1, right-1, s, dp)+2;
         else {
-            return dp[start][end] = max(func(start+1, end, s, dp), func(start, end-1, s, dp));
+            return dp[left][right] = max(func(left+1, right, s, dp), func(left, right-1, s, dp));
         }
+        
         return 0;
     }
     int longestPalindromeSubseq(string s) {
         
-        // vector<vector<int>>dp(s.length()+1, vector<int>(s.length()+1, -1))
-        int len = s.length();
-        int dp[len+1][len+1];
-        
-        for(int i = 0; i <= len; i++) 
-            dp[0][i] = 0,dp[i][len] = 0 ;
-        
-        for(int i = 1; i <= len; i++) {
-            for(int j = len-1; j >= 0; j--) {
-                if(s[i-1] == s[j])
-                    dp[i][j] = dp[i-1][j+1] + 1;
-                else
-                    dp[i][j] = max(dp[i-1][j], dp[i][j+1]);
-            }
-        }
-        return dp[len][0];
+        // vector<vector<int>>dp(s.length(), vector<int>(s.length(), -1));
         // return func(0, s.length()-1, s, dp);
+        
+        int dp[s.length()][s.length()];
+        
+        for(int i = 0; i < s.length(); i++) 
+            dp[i][i] = 1;
+        for(int i = 0; i < s.length()-1; i++) 
+            if(s[i] == s[i+1])
+                dp[i][i+1] = 2;
+            else
+                dp[i][i+1] = 1;
+        
+        // for(int i = 0; i < s.length(); i++) {
+            for(int len = 2; len < s.length(); len++) {
+                
+                for(int i=0,j=len; j < s.length(); i++, j++) {
+                    
+                    if(s[i] == s[j]) 
+                        dp[i][j] = dp[i+1][j-1] + 2;
+                    else {
+                        dp[i][j] = max(dp[i+1][j], dp[i][j-1]);
+                    }
+                    
+                }
+            }
+        
+        return dp[0][s.length()-1];
+        
     }
 };
