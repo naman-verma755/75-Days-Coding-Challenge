af686bb3d7f5a35919707ae6dc8eb51e9cd30bcf
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-07-24 20:11:51 +0530

    Time: 36 ms (11.63%), Space: 9.2 MB (17.21%) - LeetHub

--- 402-remove-k-digits/402-remove-k-digits.cpp
+++ 402-remove-k-digits/402-remove-k-digits.cpp
@@ -1,48 +1,41 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        string ans="";
-        for(char &c:num)
-        {
-            while(ans.size() && ans.back()>c &&k)
-            {
-                ans.pop_back();
+
+        stack<char>st;
+        for(int i = 0; i < num.length(); i++) {
+            
+            while(!st.empty() && st.top() > num[i] && k > 0) {
+                st.pop();
                 k--;
             }
-            if(ans.size() || c!='0')ans.push_back(c);
-        }
-        while(ans.size()&&k--)          // <-------this look is to make sure we fulfil the deletion condition
-        {
-            ans.pop_back();
-        }
-        return (ans=="")?"0":ans;
-//         stack<char>st;
-        
-//         for(char ch: num) {
             
-//              while(!st.empty() && ch < st.top() && k > 0) 
-//                  st.pop(),k--;
-            
-//             if(ch == '0' && st.size() == 0)
-//                 continue;
-//             st.push(ch);
-//         }
-// //         // cout<<st.top();
+            st.push(num[i]);
+        }
         
-//         string ans = "";
-//         while(!st.empty()) {
-//             ans = st.top()+ans;
-//             st.pop();
-//         }
-//         // ans = ans.substr(0, ans.length()-k);
         
-//         while(ans.size()&&k--)           
-//         {
-//             ans.pop_back();
-//         }
-//         if(ans == "")
-//             return "0";
-//         return ans;
-    
+        
+        if(st.empty())
+            return "0";
+        // while(!st.empty() && st.top() == '0')
+        //     st.pop();
+        
+        string ans = "";
+        while(!st.empty()) {
+            ans += st.top();
+            st.pop();
+        }
+         reverse(ans.begin(), ans.end());
+        while( k>0 )
+            ans.pop_back(),k--;
+        int i = 0;
+        while(i < ans.length() && ans[i] == '0')
+            i++;
+        ans = ans.substr(i, ans.length());
+       
+        if(ans == "")
+            return "0";
+        
+        return ans;
     }
 };
