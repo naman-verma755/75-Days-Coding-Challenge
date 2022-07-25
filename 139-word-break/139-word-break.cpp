eb04bf86f75dd9837fae1dae10c27e0edc3bd618
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-07-25 19:58:15 +0530

    Time: 16 ms (57.15%), Space: 9.7 MB (59.90%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
-    bool func(string s, set<string>&st, map<string, bool>&mp) {
+    
+    bool func(string s, set<string>&st, map<string, int>&mp) {
         
         if(s.length() == 0)
-            return false;
-        if(st.find(s) != st.end())
             return true;
+        
         if(mp.count(s))
             return mp[s];
-        string temp = "";
+        string re = "";
+        
         for(int i = 0; i < s.length(); i++) {
-            temp += s[i];
-            if(st.find(temp) != st.end() && func(s.substr(i+1, s.length()), st, mp))
-               return mp[s] = true;
+            re += s[i];
+            if(st.find(re) != st.end()) {
+                if(func(s.substr(i+1), st, mp))
+                    return mp[s] = true;
+            }
         }
         return mp[s] = false;
     }
     bool wordBreak(string s, vector<string>& wordDict) {
         
         set<string>st;
-        map<string, bool>mp;
-        for(string word : wordDict) {
+        map<string, int>mp;
+        for(string word : wordDict)
             st.insert(word);
-        }
-       return func(s, st, mp);
+        
+        return func(s, st, mp);
     }
 };
