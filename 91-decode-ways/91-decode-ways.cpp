c7b101a758a82842646f41bcbce0a157fdb407a0
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-07-25 17:46:20 +0530

    Time: 3 ms (60.67%), Space: 7.4 MB (18.54%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,46 +1,29 @@
 class Solution {
 public:
-    int decode(int index, string s, vector<int>&dp) {
+    
+    int func(int index, string s, vector<int>&dp) {
         
-        if(index == s.length())
+        if(index >= s.length())
             return 1;
-        string ans = "";
-        ans += s[index];
-        if(ans == "0")
+        
+        if(s[index] == '0')
             return 0;
+        
         if(dp[index] != -1)
             return dp[index];
-        int way1 = decode(index+1, s, dp), way2 = 0;
-        if(index+1 < s.length()) {
-            ans += s[index+1];
-            if(ans >= "1" && ans <= "26") 
-            way2 = decode(index+2, s, dp);
-        }
-        return dp[index] = way1+way2;
+        int way1 = func(index+1, s, dp);
+        int way2 = 0;
+        string st = s.substr(index, 2);
+        // cout<<st<<" ";
+        if(index+1 < s.length() && st >= "1" && st <= "26")
+            way2 = func(index+2, s, dp);
         
+        return dp[index] = way1+way2;
     }
     int numDecodings(string s) {
         
-        vector<int>dp(s.length()+1);
-        if(s[0] == '0')
-            return 0;
+        vector<int>dp(s.length(), -1);
         
-        dp[0] = 1;
-        dp[1] = 1;
-        for(int i = 2; i <= s.length(); i++) {
-            int way1 = 0, way2 = 0;
-            string ans = "";
-            ans += s[i-1];
-            if(ans != "0")
-                way1 = dp[i-1];
-            ans = s[i-2]+ans;
-            if(ans >= "1" && ans <= "26")
-                way2 = dp[i-2];
-            
-            dp[i] = way1+way2;
-        }
-        return dp[s.length()];
-//         return decode(0, s, dp);
-        
+        return func(0, s, dp);
     }
 };
