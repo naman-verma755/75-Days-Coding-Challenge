a368a0d0e768087f7e1abdf9fda114e230a19151
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-07-26 18:10:13 +0530

    Time: 25 ms (10.22%), Space: 17.3 MB (51.60%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -13,30 +13,26 @@ class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
         
-        
         queue<pair<TreeNode*, long long>>q;
-        
-        
         q.push({root, 0});
-        long long maxi = 0;
+        int maxi = 0;
         while(!q.empty()) {
-            int size = q.size();
-            long long leftMost = q.front().second;
             
-            while(size > 0) {
-                
-                TreeNode* node = q.front().first;
-                long long level = q.front().second;
+            int k = q.size();
+            int leftMost = q.front().second;
+            while(k > 0) {
+                TreeNode *node = q.front().first;
+               long long level = q.front().second;
+                maxi = max(maxi,(int)(level-leftMost+1));
                 q.pop();
-                long long k = level-leftMost;
-                if(node->left != NULL) {
-                    q.push({node->left, (2*k+1)});
-                }
-                if(node->right != NULL) {
-                    q.push({node->right, (2*k+2)});
-                }
-                maxi = max(maxi, (level-leftMost+1));
-                size--;
+                long long r = level-leftMost;
+                if(node->left != NULL) 
+                    q.push({node->left, (2*r+1)});
+                if(node->right != NULL)
+                    q.push({node->right, (2*r+2)});
+                
+                k--;
+                
             }
         }
         return maxi;
