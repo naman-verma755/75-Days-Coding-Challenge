387448ee495432e224d9e5348d135c16c5c9b806
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-08-01 22:29:57 +0530

    Time: 17 ms (42.86%), Space: 10.1 MB (53.11%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,23 +1,38 @@
 class Solution {
 public:
-    
-    int minPathSum(vector<vector<int>>& grid) {
-        int rows = grid.size();
-        int columns = grid[0].size();
-        int dp[rows][columns];
-        dp[0][0] = grid[0][0];
-        for(int col = 1; col < columns; col ++)
-            grid[0][col] = grid[0][col] + grid[0][col-1];
-        for(int row = 1; row < rows; row++) 
-            grid[row][0] = grid[row][0] + grid[row-1][0];
+    int func(int i, int j, int n, int m, vector<vector<int>>& grid, vector<vector<int>>&dp) {
+       
+        if(i == n-1 && j == m-1)
+            return grid[i][j];
         
-        for(int row = 1; row < rows; row++) {
-            for(int col = 1; col < columns; col++) {
-                grid[row][col] = grid[row][col] + min(grid[row-1][col],grid[row][col-1]);
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        int down = INT_MAX,right = INT_MAX;
+        
+        if(i+1 < n)
+            down = func(i+1, j, n, m, grid, dp);
+        if(j+1 < m)
+            right = func(i, j+1, n, m, grid, dp);
+        return dp[i][j] = min(down, right)+grid[i][j];
+    }
+    int minPathSum(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<int>>dp(n+1, vector<int>(m+1));
+        // dp[n-1][m-1] = grid[n-1][m-1];
+        // return func(0, 0, n, m, grid, dp);
+        for(int i = n-1; i >= 0; i--) {
+            for(int j = m-1; j >= 0; j--) {
+                int k = INT_MAX;
+                dp[i][j] = 0;
+                if(i+1 < n)
+                    k = min(k, dp[i+1][j]);
+                if(j+1 < m)
+                    k = min(k, dp[i][j+1]);
+                if(k != INT_MAX)
+                    dp[i][j] = k;
+                dp[i][j] += grid[i][j];
             }
         }
-        
-        return grid[rows-1][columns-1];
-   
+        return dp[0][0];
     }
 };
