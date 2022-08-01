34177882663f2c41faef715c1408051b1de0a047
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-08-01 20:39:36 +0530

    Time: 552 ms (27.00%), Space: 18 MB (9.34%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,32 +1,21 @@
 class Solution {
 public:
-    int jump(vector<int>& nums) {
-        
-        int size = nums.size();
-        if(size == 1)
+    int func(int index, vector<int>&nums, vector<int>&dp) {
+        if(index >= nums.size()-1 )
             return 0;
-        int path[size];
-        // memset(path, INT_MAX, sizeof(p0
-        for(int i = 0; i < size; i++)
-            path[i] = INT_MAX;
-        path[0] = 0;
-        for(int i = 0; i < size; i++) {
-            
-            // path[i] = 1;
-            for(int j = nums[i]+i; j > i ; j--) {
-                // cout<<"o="<<i<<" "<<path[i]<<endl;
-                if(j >= size-1)
-                    return path[i]+1;
-                
-                path[j] = min(path[j], path[i]+ 1);
-                
-            }
-            // for(int k = 0; k < size; k++)
-            //    cout<<path[k]<<" ";
-            // cout<<endl;
-           
+        if(dp[index] != -1)
+            return dp[index];
+        int mini = INT_MAX, minSteps;
+        for(int i = 1; i <= nums[index]; i++) {
+            minSteps = func(index+i, nums, dp);
+            mini = min(mini, minSteps);
         }
-        return path[size-1];
+        return dp[index] = (mini == INT_MAX)?mini:mini+1;
+        
+    }
+    int jump(vector<int>& nums) {
+        vector<int>dp(nums.size()+3, -1);
+        return func(0, nums, dp);
         
     }
 };
