c99aa1f0ac6b60ce29f8b9c78224a4e9a3b3526c
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-08-01 22:30:30 +0530

    Time: 11 ms (80.93%), Space: 9.9 MB (70.36%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    int func(int i, int j, int n, int m, vector<vector<int>>& grid, vector<vector<int>>&dp) {
+//     int func(int i, int j, int n, int m, vector<vector<int>>& grid, vector<vector<int>>&dp) {
        
-        if(i == n-1 && j == m-1)
-            return grid[i][j];
+//         if(i == n-1 && j == m-1)
+//             return grid[i][j];
         
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        int down = INT_MAX,right = INT_MAX;
+//         if(dp[i][j] != -1)
+//             return dp[i][j];
+//         int down = INT_MAX,right = INT_MAX;
         
-        if(i+1 < n)
-            down = func(i+1, j, n, m, grid, dp);
-        if(j+1 < m)
-            right = func(i, j+1, n, m, grid, dp);
-        return dp[i][j] = min(down, right)+grid[i][j];
-    }
+//         if(i+1 < n)
+//             down = func(i+1, j, n, m, grid, dp);
+//         if(j+1 < m)
+//             right = func(i, j+1, n, m, grid, dp);
+//         return dp[i][j] = min(down, right)+grid[i][j];
+//     }
     int minPathSum(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size();
-        vector<vector<int>>dp(n+1, vector<int>(m+1));
-        // dp[n-1][m-1] = grid[n-1][m-1];
-        // return func(0, 0, n, m, grid, dp);
+        
+        int dp[n][m];
         for(int i = n-1; i >= 0; i--) {
             for(int j = m-1; j >= 0; j--) {
                 int k = INT_MAX;
