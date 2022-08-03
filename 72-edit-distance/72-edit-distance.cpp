53c04957cdc9a5e9c076da160979d5c95146e6e4
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-08-03 10:56:35 +0530

    Time: 13 ms (78.28%), Space: 6.5 MB (91.44%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -21,21 +21,23 @@ public:
     int minDistance(string word1, string word2) {
         // vector<vector<int>>dp(word1.length(), vector<int>(word2.length(), -1));
         int len1 = word1.length(), len2 = word2.length();
-        int dp[len1+1][len2+1];
+        // int dp[len1+1][len2+1];
+        vector<int>prev(len2+1);
+        vector<int>curr(len2+1);
         for(int i = 0; i <= len2; i++)
-            dp[0][i] = i;
-        for(int i = 0; i <= len1; i++)
-            dp[i][0] = i;
+            prev[i] = i;
         for(int i = 1; i <= len1; i++) {
+            curr[0] = i;
             for(int j = 1; j <= len2; j++) {
                 if(word1[i-1] == word2[j-1]) {
-                    dp[i][j] = dp[i-1][j-1];
+                    curr[j] = prev[j-1];
                 }
                 else {
-                    dp[i][j] = min(dp[i-1][j-1], min(dp[i-1][j], dp[i][j-1]))+1;
+                    curr[j] = min(prev[j-1], min(prev[j], curr[j-1]))+1;
                 }
             }
+            prev = curr;
         }
-        return dp[len1][len2];
+        return prev[len2];
     }
 };
