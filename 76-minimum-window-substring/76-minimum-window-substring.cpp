a7c9958113f4ea65d9ff096dd639b954b7d6c90d
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-08-09 21:09:28 +0530

    Time: 88 ms (18.83%), Space: 8.1 MB (36.64%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -1,41 +1,38 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        map<char, int>mp1, mp2;
-        for(char ch : t)
-            mp1[ch]++;
         
-        int count = 0;
-        int i = 0, j =0;
+        map<char, int>mp1, mp2;
+          int maxi = INT_MAX;
         string ans = "";
-        int len = INT_MAX;
-        while(j < s.length()) {
-            
-            if(mp1.count(s[j]))
-                mp2[s[j]]++;
-            if(mp1.count(s[j]) && mp2[s[j]] <= mp1[s[j]])
-                count++;
+        for(char ch: t)
+            mp1[ch]++;
+        int count = 0;
+        int itr1 = 0, itr2 = 0;
+      
+        while(itr1 < s.length()) {
+            if(mp1.count(s[itr1]))
+            mp2[s[itr1]]++;
+            if(mp1.count(s[itr1]) && mp2[s[itr1]] <= mp1[s[itr1]])
+            count++;
             
             if(count == t.length()) {
-                // cout<<"ye";
-                 
-                // cout<<i<<" "<<j<<endl;
-                while(!mp1.count(s[i]) || mp2[s[i]] > mp1[s[i]]) {
+                // cout<<itr1<<" ";
+                while(!mp1.count(s[itr2]) || mp2[s[itr2]] > mp1[s[itr2]]) {
                     
-                    if(mp1.count(s[i]) && mp2[s[i]] > mp1[s[i]])
-                        mp2[s[i]]--;
-                    
-                    i++;
-                }
-                if(j-i+1 < len) {
-                    len = j-i+1;
-                    ans = s.substr(i, j-i+1);
+                    if(mp1.count(s[itr2]) && mp2[s[itr2]] > mp1[s[itr2]])
+                        mp2[s[itr2]]--;
+                    itr2++;
                 }
                 
-               
-                
+                 if(maxi > (itr1-itr2+1)) {
+                maxi = itr1-itr2+1;
+                ans = s.substr(itr2, (itr1-itr2+1));
+                  }
             }
-            j++;
+           
+            itr1++;
+            
         }
         return ans;
     }
