44bc6512ba2d13e6dcd61034925c674943c2cc6d
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-08-09 19:48:33 +0530

    Time: 662 ms (55.92%), Space: 184.4 MB (5.35%) - LeetHub

--- 146-lru-cache/146-lru-cache.cpp
+++ 146-lru-cache/146-lru-cache.cpp
@@ -1,77 +1,72 @@
 struct Node{
+    public:
+ 
+    int val;
     int key;
-    int value;
     Node *left;
     Node *right;
-    
-    Node(int key_, int value_) {
-        key = key_;
-        value = value_;
-        left = right = NULL;
+    Node(int _key, int _val) {
+        key = _key;
+        val = _val;
     }
 };
 
 class LRUCache {
 public:
-    
-    int size = 0;
+    int capacity;
+    int size;
+    Node *head;
+    Node *tail;
     map<int, Node*>mp;
-    Node *head = new Node(-1, -1);
-    Node *tail = new Node(-1,-1);
-    LRUCache(int capacity) {
-        size = capacity;
+    LRUCache(int _capacity) {
+        size = 0;
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+        capacity = _capacity;
         head->right = tail;
         tail->left = head;
-    }
-    
-    void insert(int key, int value) {
         
-        // head->right = tail;
+    }
+    void deleteNode(int _key) {
+      
+        Node *currNode = mp[_key];
+        mp.erase(_key);
+        Node *leftNode = currNode->left;
+        Node *rightNode = currNode->right;
+        currNode->left = NULL;
+        currNode->right = NULL;
+        leftNode->right = rightNode;
+        rightNode->left = leftNode;
+        
+    }
+    void insertNode(int key, int value) {
+        
         Node *newNode = new Node(key, value);
         mp[key] = newNode;
         Node *temp = head->right;
         head->right = newNode;
         newNode->left = head;
-        newNode->right = temp;
         temp->left = newNode;
+        newNode->right = temp;
     }
-    void deleteKey(int key) {
-        
-        Node* currNode = mp[key];
-        mp.erase(key);
-        Node*leftNode = currNode->left;
-        Node*rightNode = currNode->right;
-        currNode->left = currNode->right = NULL;
-        delete(currNode);
-        leftNode->right = rightNode;
-        rightNode->left = leftNode;
-        
-        
-    }
+    
     int get(int key) {
         
         if(!mp.count(key))
             return -1;
-        int value = mp[key]->value;
-        deleteKey(key);
-        
-        insert(key, value);
-        
-        return value;
+        int val = mp[key]->val;
+        deleteNode(key);
+        insertNode(key, val);
+        return val;
     }
     
     void put(int key, int value) {
         
-         if(mp.count(key)) {
-            deleteKey(key);
-        }
-        
-        if(mp.size()+1 > size) {
-            deleteKey(tail->left->key);
-        }
-       
-        insert(key, value);
-        
+        if(mp.count(key))
+            deleteNode(key);
+        else if(mp.size() == capacity)
+            deleteNode(tail->left->key);
+        insertNode(key, value);
     }
 };
 
