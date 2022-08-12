836cb4405e94798ac12eb4451806581bca294e48
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-08-12 22:45:18 +0530

    Time: 43 ms (80.47%), Space: 70.9 MB (93.69%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -10,15 +10,8 @@
  */
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        
-          ListNode *ans = l1;
-          ListNode *prev1, *prev2;
-          int carry = 0;
-          while(l1 != NULL && l2 != NULL) {
-              int val = l1->val+l2->val + carry;
-              carry = 0;
-              if(val > 9)
+    void assign(int val, int &carry, ListNode *l1) {
+        if(val > 9)
               {
                   int k = val%10;
                   val /= 10;
@@ -27,6 +20,17 @@ public:
               }
               else
                   l1->val = val;
+    }
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        
+          ListNode *ans = l1;
+          ListNode *prev1, *prev2;
+          int carry = 0;
+          while(l1 != NULL && l2 != NULL) {
+              int val = l1->val+l2->val + carry;
+              carry = 0;
+              assign(val, carry, l1);
+              
               prev1 = l1;
               prev2 = l2;
               l1 = l1->next;
@@ -40,15 +44,7 @@ public:
           while(l1 != NULL) {
               int val = l1->val + carry;
               carry = 0;
-              if(val > 9)
-              {
-                  int k = val%10;
-                  val /= 10;
-                  carry = val%10;
-                  l1->val = k;
-              }
-              else
-                  l1->val = val;
+              assign(val, carry, l1);
               prev1 = l1;
               l1 = l1->next;
           }
