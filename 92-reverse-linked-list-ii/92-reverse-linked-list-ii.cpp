b4168dedb6f07ecbd38d6180f4525ca7c1769329
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-08-12 22:59:29 +0530

    Time: 7 ms (23.65%), Space: 7.5 MB (18.07%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
@@ -10,31 +10,65 @@
  */
 class Solution {
 public:
+    ListNode* rever(ListNode *node, int count) {
+        
+        ListNode *head = node, *prev = NULL;
+        int c = 1;
+        while(c <= count) {
+            ListNode *temp = node->next;
+            node->next = prev;
+            prev = node;
+            node = temp;
+            c++;
+        }
+        head->next = node;
+        return prev;
+        
+    }
     ListNode* reverseBetween(ListNode* head, int left, int right) {
         
-        ListNode *leftNode, *leftPrev, *curr = head;
-        int pos = 1;
-        while(pos < left){
-            leftPrev = curr;
-            curr = curr->next;
-            pos += 1;
+        if(left == 1)
+            return rever(head, right-left+1);
+        ListNode *node = head;
+        int pos = 2;
+        while(pos < left) {
+            pos++;
+            node = node->next;
         }
+        node->next = rever(node->next, right-left+1);
         
-        leftNode = curr;
-        ListNode *prev = NULL, *temp = NULL;
-        while(pos < right+1) {
-            temp = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = temp;
-            pos += 1;
-        }
-         leftNode->next = curr;
-         if(left == 1)
-            return prev;
-        leftPrev->next = prev;
-       
-       
+        
         return head;
+        
+        
+        
+        
+        
+        
+        
+//         ListNode *leftNode, *leftPrev, *curr = head;
+//         int pos = 1;
+//         while(pos < left){
+//             leftPrev = curr;
+//             curr = curr->next;
+//             pos += 1;
+//         }
+        
+//         leftNode = curr;
+//         ListNode *prev = NULL, *temp = NULL;
+//         while(pos < right+1) {
+//             temp = curr->next;
+//             curr->next = prev;
+//             prev = curr;
+//             curr = temp;
+//             pos += 1;
+//         }
+//          leftNode->next = curr;
+//          if(left == 1)
+//             return prev;
+//         leftPrev->next = prev;
+       
+       
+//         return head;
     }
 };
