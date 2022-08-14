035febee4dd75ebb2b91c9f25af4df94c5f1d05b
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-08-14 11:22:23 +0530

    Time: 147 ms (6.05%), Space: 22.5 MB (8.02%) - LeetHub

--- 909-snakes-and-ladders/909-snakes-and-ladders.cpp
+++ 909-snakes-and-ladders/909-snakes-and-ladders.cpp
@@ -1,82 +1,60 @@
 class Solution {
 public:
+  
     int snakesAndLadders(vector<vector<int>>& board) {
-        int n = board.size();
-        int dp[(n*n+4)];
         
-        
-        int flag = 1;
-        int index = 1;
-        for(int i = n-1; i >= 0; i--) {
-            
-            if(flag == 1) {
-                for(int j = 0; j < n; j++) 
-                {
-                    dp[index] = board[i][j];
-                    index++;
-                }
-                flag = 0;
-            }
-            else {
-                for(int j = n-1; j >= 0; j--) 
-                {
-                    dp[index] = board[i][j];
-                    index++;
-                }
-                flag = 1;
-            }
-            
-        }
-       
-        
-        queue<int>q;
-        q.push(1);
-        
-        bool visited[n*n+2];
+          int n = board.size();
+          int dp[(n*n)+1];
+          bool flag = true;
+          int k = 1;
+          for(int i = n-1; i >= 0; i--) {
+              if(flag) {
+                  flag = !flag;
+                  for(int j = 0; j < n; j++)
+                      dp[k++] = board[i][j];
+              }
+              else {
+                  flag = !flag;
+                  for(int j = n-1; j >= 0; j--) 
+                      dp[k++] = board[i][j];
+              }
+          }
+        // for(int i = 1; i <= (n*n); i++) {
+        //     cout<<dp[i]<<" ";
+        // }
+        // cout<<endl;
+          bool visited[n*n+1];
         memset(visited, false, sizeof(visited));
-        int step = 0;
-        int target = n*n;
-        while(!q.empty()) {
-            int size = q.size();
-            while(size > 0) {
-                
-                int num = q.front();
-                q.pop();
-                if(num == target)
-                    return step;
-                visited[num] = true;
-                
-                for(int i = 1; i <= 6; i++) {
-                    int next = num+i;
-                    if(next <= (n*n)) {
-                    if(dp[next] != -1)
-                        next = dp[next];
-                    // cout<<next<<" ";
-                    if( !visited[next]) 
-                        q.push(next);
-                    }
-                }
-                size--;
-            }
-            step++;
-        }
-        
+          queue<int>q;
+          q.push(1);
+        int count = 0;
+          while(!q.empty()) {
+              k = q.size();
+            
+              while(k > 0) {
+                  int top = q.front();
+                  q.pop();
+                  
+            
+                  visited[top] = true;
+               
+                  
+                  
+                  for(int j = 1; j <= 6; j++) {
+                      int pos = j+top;
+                     
+                      if( dp[pos] != -1)
+                          pos = dp[pos];
+                     
+                      if(pos >= (n*n))
+                          return count+1;
+                      if(!visited[pos])
+                      q.push(pos);
+                  }
+                  k--;
+              }
+              count++;
+          }
         return -1;
-        
-
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
     }
 };
