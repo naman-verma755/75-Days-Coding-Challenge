436c00cf08e9ab4ab5efb0bdf97b4293d77537fb
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-08-14 12:05:33 +0530

    Time: 15 ms (37.73%), Space: 13.6 MB (15.18%) - LeetHub

---
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        set<pair<int, int>>st;
+        queue<pair<int, int>>q;
+        int total = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(grid[i][j] == 1 || grid[i][j] == 2)
+                    total ++;
+                if(grid[i][j] == 2) {
+                
+                    q.push({i,j});
+                    st.insert({i,j});
+                }
+            }
+        }
+        if(total == 0)
+            return 0;
+        int time = -1;
+        int dir1[] = {-1, 1, 0, 0};
+        int dir2[] = {0, 0, -1, 1};
+        int count = 0;
+        while(!q.empty()) {
+            
+            int k = q.size();
+            count += k;
+            while(k > 0) {
+                
+                int i = q.front().first;
+                int j = q.front().second;
+                
+                q.pop();
+                
+                for(int it = 0; it < 4; it++)
+                {
+                    int x = dir1[it]+i, y = dir2[it]+j;
+                    if(x <0 || y < 0 || x >= n || y >= m)
+                        continue;
+                    if(st.find({x,y}) != st.end())
+                        continue;
+                    if(grid[x][y] == 1) {
+                        q.push({x,y});
+                        st.insert({x,y});
+                    }
+                }
+                k--;
+            }
+            time++;
+            
+        }
+        if(count == total)
+        return time;
+        else
+            return -1;
+        
+    }
+};
