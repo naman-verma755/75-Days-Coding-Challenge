903abe1463b4535c146a8ab1592fe7449e6e6b05
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-09-22 14:56:32 +0530

    Time: 20 ms (95.94%), Space: 10.6 MB (81.09%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,53 +1,42 @@
 class Solution {
 public:
+//     int func(int i, int j, string s, string t, vector<vector<int>>&dp) {
+//         if( j == t.length())
+//             return 1;
+//         if(i == s.length() || j == t.length())
+//             return 0;
+        
+//         if(dp[i][j] != -1)
+//             return dp[i][j];
+//         if(s[i] == t[j]) {
+//             return dp[i][j] = func(i+1, j+1, s, t, dp) + func(i+1, j, s, t, dp);
+//         }
+//         else
+//             return dp[i][j] = func(i+1, j, s, t, dp) ;
+        
     
-    int func(int i, int j, string s, string t, vector<vector<int>>&dp) {
-        
-        if(j == t.length())
-            return 1;
-        
-        if(i == s.length())
-            return 0;
-        
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        if(s[i] == t[j]) {
-            return dp[i][j] = func(i+1, j+1, s, t, dp)+func(i+1, j, s, t, dp);
-        }
-        else {
-            return dp[i][j] = func(i+1, j, s, t, dp);
-        }
-    }
+//     }
     int numDistinct(string s, string t) {
+        
         // vector<vector<int>>dp(s.length(), vector<int>(t.length(), -1));
         // return func(0, 0, s, t, dp);
         int mod = 1000000007;
-        int slen = s.length();
-        int tlen = t.length();
-        // int dp[slen+1][tlen+1];
-        vector<int>prev(tlen+1, 0);
-    
-       
-//         for(int j = 0; j <= tlen; j++)
-//             dp[0][j] = 0;
-         
-//         for(int i = 0; i <= slen; i++) 
-//             dp[i][0] = 1;
-        prev[0] = 1;
-        for(int i = 1; i <= slen; i++) {
-            vector<int>curr(tlen+1, 0);
-            curr[0] = 1;
-            for(int j = 1; j <= tlen; j++) {
-                if(s[i-1] == t[j-1]) {
-                     curr[j]= (prev[j-1]+prev[j])%mod;
-                }
+        int len1 = s.length();
+        int len2 = t.length();
+        int dp[len1+1][len2+1];
+        for(int i = 0; i <= len2; i++) 
+            dp[0][i] = 0;
+        for(int i = 0; i <= len1; i++)
+            dp[i][0] = 1;
+        
+        for(int i = 1; i <= len1; i++) {
+            for(int j = 1; j <= len2; j++) {
+                if(s[i-1] == t[j-1])
+                    dp[i][j] = (dp[i-1][j-1]+dp[i-1][j])%mod;
                 else
-                    curr[j] = prev[j];
+                    dp[i][j] = dp[i-1][j];
             }
-            prev = curr;
-        
         }
-        
-        return prev[tlen];
+        return dp[len1][len2];
     }
 };
