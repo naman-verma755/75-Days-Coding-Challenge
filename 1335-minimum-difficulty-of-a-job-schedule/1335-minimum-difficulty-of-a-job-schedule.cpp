44000c7901b78a7efc49b4091b56e752e2912e80
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-09-22 18:37:19 +0530

    Time: 358 ms (10.65%), Space: 188.4 MB (5.08%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -1,56 +1,31 @@
 class Solution {
 public:
-//     int func(int i, int j, vector<int>&a, int d, int maxi, vector<vector<vector<int>>>&dp) {
-        
-//         int size = a.size();
-//         if(j == d) {
-//             while(i < size) {
-//                 maxi = max(maxi, a[i]);
-//                 i++;
-//             }
-//             return maxi;
-//         }
-//         if( i == size)
-//             return 10005;
-        
-//         if(dp[i][j][maxi] != -1)
-//             return dp[i][j][maxi];
-        
-//         maxi = max(maxi, a[i]);
-//         int k1 = func(i+1, j, a, d, maxi, dp);
-//         int k2 = func(i+1, j+1, a, d, 0, dp)+maxi;
-//         return dp[i][j][maxi] = min(k1, k2);
-//     }
-    int minDifficulty(vector<int>& jobDifficulty, int d) {
-        int size= jobDifficulty.size();
-        if(d > size)
-            return -1;
-        
-        // vector<vector<vector<int>>>dp(size+1, vector<vector<int>>(d+1, vector<int>(1005, -1)));
-        // return func(0, 0, jobDifficulty, d, 0, dp);
-         vector<int>dp(size, 0);
-        
-        for(int i = 0; i < d; i++) {
-            for(int j = 0; j < size-i; j++) {
-                 int maxi = 0, maxAmount = INT_MAX;
-                for(int k = j; k <size-i; k++) {
-                    if(i == 0)
-                        maxi = max(jobDifficulty[k], maxi);
-                    else {
-                        maxi = max(maxi, jobDifficulty[k]);
-                        maxAmount = min(maxAmount, maxi+dp[k+1]);
-                    }
-                }
-                if(i == 0)
-                    dp[j] = maxi;
-                else
-                    dp[j] = maxAmount;
+    int func(int index, int maxi, int day, vector<int>& jDiff, int D, vector<vector<vector<int>>>&dp) {
+        int n = jDiff.size();
+          if(index == n )
+            return 10005;
+        if(day == D) {
+            while(index < n){
+                maxi = max(jDiff[index], maxi);
+                index++;
             }
-            // for(auto it : dp)
-            //     cout<<it<<" ";
-            // cout<<endl;
+            return maxi;
         }
+      
+        if(dp[index][day][maxi] != -1)
+            return dp[index][day][maxi];
+        maxi = max(maxi, jDiff[index]);
+        int k1 = func(index+1, maxi, day, jDiff, D, dp);
+        int k2 = func(index+1, 0, day+1, jDiff, D,  dp) + maxi;
+        return dp[index][day][maxi] = min(k1, k2);
+    }
+    int minDifficulty(vector<int>& jobDifficulty, int d) {
         
-        return dp[0];
+        int n = jobDifficulty.size();
+        if(n < d)
+            return -1;
+        vector<vector<vector<int>>>dp(n+1, vector<vector<int>>(d+1, vector<int>(1005, -1)));
+        
+        return func(0, 0, 1, jobDifficulty, d, dp);
     }
 };
