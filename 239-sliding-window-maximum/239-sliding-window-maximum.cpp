4a17d999894f9c497fe708755ee8a936e0d923a9
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-09-23 15:29:36 +0530

    Time: 639 ms (29.09%), Space: 134.7 MB (81.69%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -2,25 +2,22 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         
-        
         deque<int>dq;
         vector<int>ans;
         for(int i = 0; i < nums.size(); i++) {
             
-            
-            while(!dq.empty() && nums[dq.back()] <= nums[i]) {
+            while(!dq.empty() && nums[i] >= nums[dq.back()]) {
                 dq.pop_back();
             }
             
             dq.push_back(i);
-            
-            if(i-dq.front() >= k) {
+            if((i-dq.front()) >= k)
                 dq.pop_front();
-            }
             
             if(i >= k-1)
                 ans.push_back(nums[dq.front()]);
         }
+        
         return ans;
     }
 };
