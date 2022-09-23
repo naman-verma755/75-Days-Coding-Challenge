7fd82ad96eeb2806a12cc396ac44692ea8bf5458
naman-verma755 <61876890+naman-verma755@users.noreply.github.com>
2022-09-23 20:33:12 +0530

    Time: 196 ms (44.59%), Space: 45.8 MB (57.57%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,12 +1,55 @@
+class Priority_queue {
+    public:
+    int a[100001];
+    int size;
+    Priority_queue() {
+        size =0;
+    }
+    int _size() {
+        return size;
+    }
+    int top(){
+        return a[0];
+    }
+    void heapify(int i) {
+        long long left = 2*i+1;
+        long long right = 2*i+2;
+        int pos = i;
+        if(left < size && a[left] < a[pos])
+            pos = left;
+        if(right < size && a[right] < a[pos])
+            pos = right;
+        
+        swap(a[pos], a[i]);
+        if(pos != i)
+        heapify(pos);
+    }
+    void pop() {
+        int ele = a[size-1];
+        size--;
+        a[0] = ele;
+        heapify(0);
+    }
+    void push(int _val) {
+        a[size] = _val;
+        int i = size++;
+        while(i > 0 && a[(i-1)/2] > a[i]){
+            swap(a[(i-1)/2], a[i]);
+            i =(i-1)/2;
+        }
+        
+    }
+};
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
         
-        priority_queue<int, vector<int>, greater<int>>pq;
+        // priority_queue<int, vector<int>, greater<int>>pq;
+        Priority_queue pq;
         for(auto it : nums) {
             pq.push(it);
             
-            if(pq.size() > k)
+            if(pq._size() > k)
                 pq.pop();
         }
         return pq.top();
